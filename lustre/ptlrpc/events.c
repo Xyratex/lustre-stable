b9b71ac9bd53334bd267331f8758aaf9ad51112a
Alexey Lyashkov <alexey_lyashkov@xyratex.com>
2011-10-12 15:56:04 +0400

    MRP-238 don't panic on aborted bulk.
    correctly return a error in case staled ZQ entry to avoid ptlrpc_check_set
    to be confused and generate panic
    LustreError: 31789:0:(client.c:1695:ptlrpc_check_set()) @@@ bulk transfer failed ...
    
    Signed-off-by: Alexey Lyashkov <alexey_lyashkov@xyratex.com>
    Reviewed-by: Andrew Perepechko <Andrew_Perepechko@xyratex.com>
    Reviewed-by: Vitaly Fertman <vitaly_fertman@xyratex.com>

--- lustre/ptlrpc/events.c
+++ lustre/ptlrpc/events.c
@@ -180,6 +180,7 @@ void client_bulk_callback (lnet_event_t *ev)
 {
         struct ptlrpc_cb_id     *cbid = ev->md.user_ptr;
         struct ptlrpc_bulk_desc *desc = cbid->cbid_arg;
+        struct ptlrpc_request   *req;
         ENTRY;
 
         LASSERT ((desc->bd_type == BULK_PUT_SINK &&
@@ -194,7 +195,7 @@ void client_bulk_callback (lnet_event_t *ev)
                ev->type, ev->status, desc);
 
         cfs_spin_lock(&desc->bd_lock);
-
+        req = desc->bd_req;
         LASSERT(desc->bd_network_rw);
         desc->bd_network_rw = 0;
 
@@ -203,14 +204,20 @@ void client_bulk_callback (lnet_event_t *ev)
                 desc->bd_nob_transferred = ev->mlength;
                 desc->bd_sender = ev->sender;
         }
+        if (ev->type == LNET_EVENT_UNLINK || ev->status != 0) {
+                /* start reconnect and resend if network error hit */
+                cfs_spin_lock(&req->rq_lock);
+                req->rq_net_err = 1;
+                cfs_spin_unlock(&req->rq_lock);
+        }
 
         /* release the encrypted pages for write */
-        if (desc->bd_req->rq_bulk_write)
+        if (req->rq_bulk_write)
                 sptlrpc_enc_pool_put_pages(desc);
 
         /* NB don't unlock till after wakeup; desc can disappear under us
          * otherwise */
-        ptlrpc_client_wake_req(desc->bd_req);
+        ptlrpc_client_wake_req(req);
 
         cfs_spin_unlock(&desc->bd_lock);
         EXIT;
