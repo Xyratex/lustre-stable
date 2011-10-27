c87dc9a3089b19bd39c9f49314ff956bf55339cf
Mikhail Pershin <tappro@whamcloud.com>
2011-10-27 08:03:33 +0400

    LU-795 osd api: Commit callback per transaction
    - ability to add commit callback per transaction in addition to
      per-device hooks. Now it is much simpler if only commit callback
      is needed.
    - rewrite commit callbacks for last_commit and new_client, add commit
      callback in seq manager
    - cleanup not-needed code: old commit callbacks, txn_keys
    - remove osd od_env_for_commit environment and env param from commit
      callbacks
    - use th_sync to mark sync operations
    
    Change-Id: If5f8f2a6d3cd2f3e77fd13c802213a181043a2d7
    Signed-off-by: Mikhail Pershin <tappro@whamcloud.com>
    Reviewed-on: http://review.whamcloud.com/1621
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/include/dt_object.h
+++ lustre/include/dt_object.h
@@ -86,6 +86,21 @@ struct dt_device_param {
 };
 
 /**
+ * Per-transaction commit callback function
+ */
+struct dt_txn_commit_cb;
+typedef void (*dt_cb_t)(struct lu_env *env, struct thandle *th,
+                        struct dt_txn_commit_cb *cb, int err);
+/**
+ * Special per-transaction callback for cases when just commit callback
+ * is needed and per-device callback are not convenient to use
+ */
+struct dt_txn_commit_cb {
+        cfs_list_t dcb_linkage;
+        dt_cb_t    dcb_func;
+};
+
+/**
  * Basic transaction credit op
  */
 enum dt_txn_op {
@@ -124,6 +139,11 @@ struct dt_device_operations {
          */
         void  (*dt_trans_stop)(const struct lu_env *env,
                                struct thandle *th);
+        /**
+         * Add commit callback to the transaction.
+         */
+        int   (*dt_trans_cb_add)(struct thandle *th,
+                                 struct dt_txn_commit_cb *dcb);
         /**
          * Return fid of root index object.
          */
@@ -566,8 +586,6 @@ static inline int dt_object_exists(const struct dt_object *dt)
 struct txn_param {
         /** number of blocks this transaction will modify */
         unsigned int tp_credits;
-        /** sync transaction is needed */
-        __u32        tp_sync:1;
 };
 
 static inline void txn_param_init(struct txn_param *p, unsigned int credits)
@@ -580,11 +598,6 @@ static inline void txn_param_credit_add(struct txn_param *p,
                                         unsigned int credits)
 {
         p->tp_credits += credits;
-}
-
-static inline void txn_param_sync(struct txn_param *p)
-{
-        p->tp_sync = 1;
 }
 
 /**
@@ -611,6 +624,8 @@ struct thandle {
         /** the last operation result in this transaction.
          * this value is used in recovery */
         __s32             th_result;
+        /** whether we need sync commit */
+        int               th_sync;
 };
 
 /**
@@ -629,8 +644,7 @@ struct dt_txn_callback {
                              struct txn_param *param, void *cookie);
         int (*dtc_txn_stop)(const struct lu_env *env,
                             struct thandle *txn, void *cookie);
-        int (*dtc_txn_commit)(const struct lu_env *env,
-                              struct thandle *txn, void *cookie);
+        void (*dtc_txn_commit)(struct thandle *txn, void *cookie);
         void                *dtc_cookie;
         __u32                dtc_tag;
         cfs_list_t           dtc_linkage;
@@ -642,7 +656,7 @@ void dt_txn_callback_del(struct dt_device *dev, struct dt_txn_callback *cb);
 int dt_txn_hook_start(const struct lu_env *env,
                       struct dt_device *dev, struct txn_param *param);
 int dt_txn_hook_stop(const struct lu_env *env, struct thandle *txn);
-int dt_txn_hook_commit(const struct lu_env *env, struct thandle *txn);
+void dt_txn_hook_commit(struct thandle *txn);
 
 int dt_try_as_dir(const struct lu_env *env, struct dt_object *obj);
 
@@ -705,11 +719,17 @@ static inline struct thandle *dt_trans_start(const struct lu_env *env,
 }
 
 static inline void dt_trans_stop(const struct lu_env *env,
-                                 struct dt_device *d,
-                                 struct thandle *th)
+                                 struct dt_device *d, struct thandle *th)
 {
         LASSERT(d->dd_ops->dt_trans_stop);
         return d->dd_ops->dt_trans_stop(env, th);
+}
+
+static inline int dt_trans_cb_add(struct thandle *th,
+                                  struct dt_txn_commit_cb *dcb)
+{
+        LASSERT(th->th_dev->dd_ops->dt_trans_cb_add);
+        return th->th_dev->dd_ops->dt_trans_cb_add(th, dcb);
 }
 /** @} dt */
 #endif /* __LUSTRE_DT_OBJECT_H */
