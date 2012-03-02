32bc7b8526096f1589bd66b532186f92f7894f15
Jinshan Xiong <jinshan.xiong@whamcloud.com>
2012-03-01 21:46:18 -0800

    LU-1163 llite: never try to invalidate a dirty page
    When ll_page_mkwrite() detects page's covering lock is being canceled,
    it should write the page back before invalidating it. Otherwise, it
    will cause data corruption.
    
    Signed-off-by: Jinshan Xiong <jinshan.xiong@whamcloud.com>
    Change-Id: I345728daacf50edc154cd434dea392410f8cc782
    Reviewed-on: http://review.whamcloud.com/2243
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Reviewed-by: Johann Lombardi <johann@whamcloud.com>

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -9452,7 +9452,8 @@ test_220() { #LU-325
 run_test 220 "the preallocated objects in MDS still can be used if ENOSPC is returned by OST with enough disk space"
 
 test_221() {
-        cp `which date` $MOUNT
+        dd if=`which date` of=$MOUNT/date oflag=sync
+        chmod +x $MOUNT/date
 
         #define OBD_FAIL_LLITE_FAULT_TRUNC_RACE  0x1401
         $LCTL set_param fail_loc=0x80001401
