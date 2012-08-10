134e49722b1143e27bac5f95770a9f85d96a230e
Vitaly Fertman <vitaly_fertman@xyratex.com>
2012-08-10 16:03:51 +0400

    MRP-395 disconnect takes too long time if there are many locks to cancel
    besides the amount of time spent on each lock cancel, there is a resched() in
    cfs_hash_for_each_relax(), i.e. disconnect or eviction may take unexpectedly
    long time.
    - do not cancel locks on disconnect_export;
    - export will be left in obd_unlinked_exports list pinned by live locks;
    - new re-connects will created other non-conflicting exports;
    - new locks will cancel obsolete locks on conflicts;
    - once all the locks on the disconnected export will be cancelled, the export
      will be destroyed on the last ref put;
    - pool shrink and recalc on server cancel obsolete locks in small portions;
    - free the namespace before obd_exports_barrier on umount;
    
    also make the proper order of events on umount:
    - disconnect export;
    - cleanup namespace, to cancel all the locks before export barrier;
    - exports barrier;
    - lprocfs_free_per_client_stats (requires nid_exp_ref_count == 0);
    - namespace_free_post is left in cleanup ensure will not get and segfault on
      absent namespace.
    
    Reviewed-by: Alexey Lyashkov <alexey_lyashkov@xyratex.com>
    Reviewed-by: Alexander Zarochentsev <Alexander_Zarochentsev@xyratex.com>

--- lustre/ldlm/ldlm_lock.c
+++ lustre/ldlm/ldlm_lock.c
@@ -1730,10 +1730,11 @@ EXPORT_SYMBOL(ldlm_lock_set_data);
 struct export_cl_data {
 	struct obd_export	*ecl_exp;
 	int			ecl_loop;
+	int                     ecl_count;
 };
 
-int ldlm_cancel_locks_for_export_cb(cfs_hash_t *hs, cfs_hash_bd_t *bd,
-                                    cfs_hlist_node_t *hnode, void *data)
+static int ldlm_cancel_locks_for_export_cb(cfs_hash_t *hs, cfs_hash_bd_t *bd,
+                                           cfs_hlist_node_t *hnode, void *data)
 
 {
 	struct export_cl_data	*ecl = (struct export_cl_data *)data;
@@ -1760,19 +1761,72 @@ int ldlm_cancel_locks_for_export_cb(cfs_hash_t *hs, cfs_hash_bd_t *bd,
 		       cfs_atomic_read(&hs->hs_count));
 	}
 
+        /* break */
+        if (ecl->ecl_loop == ecl->ecl_count)
+                return 1;
+
 	return 0;
 }
 
-void ldlm_cancel_locks_for_export(struct obd_export *exp)
+static int ldlm_cancel_locks_for_export(struct obd_export *exp, int count)
 {
 	struct export_cl_data	ecl = {
 		.ecl_exp	= exp,
+		.ecl_count      = count,
 		.ecl_loop	= 0,
 	};
 
 	cfs_hash_for_each_empty(exp->exp_lock_hash,
 				ldlm_cancel_locks_for_export_cb, &ecl);
+
+        return ecl.ecl_loop;
 }
+
+/**
+ * \param obd : take list of exports from obd_unlinked_exports on this obd
+ * \param count : count of locks to cancel; 0 means ALL
+ *
+ * returns amount of locks canceled.
+ **/
+int ldlm_cancel_stale_locks(struct obd_device *obd, int count)
+{
+        struct obd_export *exp;
+        int cancel = count;
+        int total = 0;
+        int handled = 0;
+        int exp_tot;
+
+        LASSERT(count > 0);
+
+        cfs_spin_lock(&obd->obd_dev_lock);
+        exp_tot = obd->obd_num_unlinked;
+
+        while (cancel > 0 && exp_tot > handled) {
+                cfs_list_for_each_entry(exp, &obd->obd_unlinked_exports,
+                                        exp_obd_chain) {
+                        if (cfs_atomic_read(&exp->exp_locks_count))
+                                break;
+                }
+                /* all exports have no locks. */
+                if (&exp->exp_obd_chain == &obd->obd_unlinked_exports)
+                        break;
+
+                handled++;
+                cfs_list_move_tail(&exp->exp_obd_chain,
+                                   &obd->obd_unlinked_exports);
+                class_export_get(exp);
+                cfs_spin_unlock(&obd->obd_dev_lock);
+
+                total += ldlm_cancel_locks_for_export(exp, cancel);
+
+                class_export_put(exp);
+                cfs_spin_lock(&obd->obd_dev_lock);
+                cancel = count - total;
+        }
+        cfs_spin_unlock(&obd->obd_dev_lock);
+        return total;
+}
+EXPORT_SYMBOL(ldlm_cancel_stale_locks);
 
 /**
  * Downgrade an exclusive lock.
