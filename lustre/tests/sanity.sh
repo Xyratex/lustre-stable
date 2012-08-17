0db05413bf0fa77adb24654714ce157aa86bf040
Hongchao Zhang <hongchao.zhang@whamcloud.com>
2012-08-17 16:25:32 +0800

    LU-582 test: fix bug in get_ost_param in sanity.sh
    in get_ost_param(), the ouput should be gl_sum, which is
    the sum of 'gl' in all OSS.
    
    Signed-off-by: Hongchao Zhang <hongchao.zhang@whamcloud.com>
    Change-Id: I51d3aa591c3ca875f8a559974cf05d250b0c032a
    Reviewed-on: http://review.whamcloud.com/3693
    Reviewed-by: Jian Yu <jian.yu@intel.com>
    Tested-by: Jian Yu <jian.yu@intel.com>
    Tested-by: Hudson
    Reviewed-by: Fan Yong <fan.yong@intel.com>
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -7279,7 +7279,7 @@ get_ost_param() {
                 [ x$gl = x"" ] && gl=0
                 gl_sum=$((gl_sum + gl))
         done
-        echo $gl
+        echo $gl_sum
 }
 
 som_mode_switch() {
