44f75927e80b8515dba40d62e6cccf68161482a3
Kyrylo Shatskyy <kyrylo_shatskyy@xyratex.com>
2012-09-28 17:29:38 +0300

    MRP-369 The metadata-updates cannot be executed
    Fixed the order of -machinefile option inclusion
    in the execution command line
    
    Change-Id: I65c2d59c70f46c25f031c08ceaa0d9f96a1491bc
    Signed-off-by: Kyrylo Shatskyy <kyrylo_shatskyy@xyratex.com>
    Reviewed-on: http://morpheus.xyus.xyratex.com:8443/89
    Reviewed-by: Roman Grigoryev <Roman_Grigoryev@xyratex.com>
    Tested-by: Jenkins
    Reviewed-by: Denis Kondratenko <Denis_Kondratenko@xyratex.com>
    Reviewed-by: Vitaly Fertman <Vitaly_Fertman@xyratex.com>

--- lustre/tests/metadata-updates.sh
+++ lustre/tests/metadata-updates.sh
@@ -254,7 +254,7 @@ if [ -f "$WRITE_DISJOINT" ]; then
     set $TRACE
     MACHINEFILE=${MACHINEFILE:-$TMP/$(basename $0 .sh).machines}
     generate_machine_file $NODES_TO_USE $MACHINEFILE
-    mpi_run -np $(get_node_count ${NODES_TO_USE//,/ }) -machinefile $MACHINEFILE \
+    mpi_run -machinefile $MACHINEFILE -np $(get_node_count ${NODES_TO_USE//,/ }) \
     $WRITE_DISJOINT -f $WRITE_DISJOINT_FILE -n $NUMLOOPS || STATUS=1
 else
     skip_env "$0 : write_disjoint not found "
