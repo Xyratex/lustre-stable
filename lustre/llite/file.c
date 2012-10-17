f9128c6b857e5139ab13912d0afba8f5d020d20b
Andreas Dilger <adilger@whamcloud.com>
2012-10-17 12:42:17 -0600

    LU-2156 debug: quiet overly verbose console errors
    A number of error messages are printed on the console that do not
    represent error conditions, or print the message in an overly
    confusing manner.  One in an ongoing series of patches to clean
    up the console errors.
    
    Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
    Change-Id: Ie830cfb49e390ca4da1f4b9ed7187d421e74f12f
    Reviewed-on: http://review.whamcloud.com/4286
    Reviewed-by: Li Wei <wei.g.li@intel.com>
    Tested-by: Hudson
    Reviewed-by: Christopher J. Morrone <chris.morrone.llnl@gmail.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Tested-by: Maloo <whamcloud.maloo@gmail.com>

--- lustre/llite/file.c
+++ lustre/llite/file.c
@@ -2221,15 +2221,13 @@ static int ll_inode_revalidate_fini(struct inode *inode, int rc) {
                 if (!S_ISREG(inode->i_mode) &&
                     !S_ISDIR(inode->i_mode))
                         return 0;
-        }
+	} else if (rc != 0) {
+		CERROR("%s: revalidate FID "DFID" error: rc = %d\n",
+		       ll_get_fsname(inode->i_sb, NULL, 0),
+		       PFID(ll_inode2fid(inode)), rc);
+	}
 
-        if (rc) {
-                CERROR("failure %d inode %lu\n", rc, inode->i_ino);
-                return -abs(rc);
-
-        }
-
-        return 0;
+	return rc;
 }
 
 int __ll_inode_revalidate_it(struct dentry *dentry, struct lookup_intent *it,
@@ -2241,10 +2239,7 @@ int __ll_inode_revalidate_it(struct dentry *dentry, struct lookup_intent *it,
         int rc = 0;
         ENTRY;
 
-        if (!inode) {
-                CERROR("REPORT THIS LINE TO PETER\n");
-                RETURN(0);
-        }
+        LASSERT(inode != NULL);
 
         CDEBUG(D_VFSTRACE, "VFS Op:inode=%lu/%u(%p),name=%s\n",
                inode->i_ino, inode->i_generation, inode, dentry->d_name.name);
