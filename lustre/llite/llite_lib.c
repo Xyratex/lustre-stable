15ad198464a061b7e122574a2f9b16a226f68eb4
Christopher J. Morrone <morrone2@llnl.gov>
2012-12-17 11:39:21 -0800

    LU-709 build: remove old BGL support option
    Remove the old and obsolete --enable-bgl configure option.
    Lustre no longer supports the old kernel that BG/L systems
    use, and we do not use this opton on the later BG/P and
    BG/Q systems.
    
    Change-Id: Iea0b893fd67fe5242fa65ebc8993c1f2b0863ae6
    Signed-off-by: Christopher J. Morrone <morrone2@llnl.gov>
    Reviewed-on: http://review.whamcloud.com/4841
    Tested-by: Hudson
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Yang Sheng <yang.sheng@intel.com>

--- lustre/llite/llite_lib.c
+++ lustre/llite/llite_lib.c
@@ -96,11 +96,7 @@ static struct ll_sb_info *ll_init_sbi(void)
         si_meminfo(&si);
         pages = si.totalram - si.totalhigh;
         if (pages >> (20 - CFS_PAGE_SHIFT) < 512) {
-#ifdef HAVE_BGL_SUPPORT
-                sbi->ll_async_page_max = pages / 4;
-#else
                 sbi->ll_async_page_max = pages / 2;
-#endif
         } else {
                 sbi->ll_async_page_max = (pages / 4) * 3;
         }
