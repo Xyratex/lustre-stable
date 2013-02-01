a940a0fcabe063a4f0f535186a9557ba2780ebd7
Bobi Jam <bobijam.xu@intel.com>
2013-02-01 09:47:33 +0800

    LU-2726 procfs: Invalid assertion for counter_{add,sub}
    lprocfs_stats::lc_sum_irq is only used for obd_memory for accurate
    memory usage counting, other procfs stats counters do not need it,
    even though they can be used in RCU callbacks.
    
    Signed-off-by: Bobi Jam <bobijam.xu@intel.com>
    Change-Id: I4c2dec50eecc0c63631f1915ba30a54a5da2fec1
    Reviewed-on: http://review.whamcloud.com/5237
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: John Hammond <johnlockwoodhammond@gmail.com>
    Tested-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/lvfs/lvfs_lib.c
+++ lustre/lvfs/lvfs_lib.c
@@ -79,9 +79,6 @@ void lprocfs_counter_add(struct lprocfs_stats *stats, int idx, long amount)
 	if (stats == NULL)
 		return;
 
-	LASSERT(ergo((stats->ls_flags & LPROCFS_STATS_FLAG_IRQ_SAFE) == 0,
-		     !cfs_in_interrupt()));
-
 	/* With per-client stats, statistics are allocated only for
 	 * single CPU area, so the smp_id should be 0 always. */
 	smp_id = lprocfs_stats_lock(stats, LPROCFS_GET_SMP_ID, &flags);
@@ -98,8 +95,13 @@ void lprocfs_counter_add(struct lprocfs_stats *stats, int idx, long amount)
 		 * as memory allocation could trigger memory shrinker call
 		 * ldlm_pool_shrink(), which calls lprocfs_counter_add().
 		 * LU-1727.
+		 *
+		 * Only obd_memory uses LPROCFS_STATS_FLAG_IRQ_SAFE
+		 * flag, because it needs accurate counting lest memory leak
+		 * check reports error.
 		 */
-		if (cfs_in_interrupt())
+		if (cfs_in_interrupt() &&
+		    (stats->ls_flags & LPROCFS_STATS_FLAG_IRQ_SAFE) != 0)
 			percpu_cntr->lc_sum_irq += amount;
 		else
 			percpu_cntr->lc_sum += amount;
@@ -125,9 +127,6 @@ void lprocfs_counter_sub(struct lprocfs_stats *stats, int idx, long amount)
 	if (stats == NULL)
 		return;
 
-	LASSERT(ergo((stats->ls_flags & LPROCFS_STATS_FLAG_IRQ_SAFE) == 0,
-		     !cfs_in_interrupt()));
-
 	/* With per-client stats, statistics are allocated only for
 	 * single CPU area, so the smp_id should be 0 always. */
 	smp_id = lprocfs_stats_lock(stats, LPROCFS_GET_SMP_ID, &flags);
@@ -143,8 +142,13 @@ void lprocfs_counter_sub(struct lprocfs_stats *stats, int idx, long amount)
 		 * softirq context - right now that's the only case we're in
 		 * softirq context here, use separate counter for that.
 		 * bz20650.
+		 *
+		 * Only obd_memory uses LPROCFS_STATS_FLAG_IRQ_SAFE
+		 * flag, because it needs accurate counting lest memory leak
+		 * check reports error.
 		 */
-		if (cfs_in_interrupt())
+		if (cfs_in_interrupt() &&
+		    (stats->ls_flags & LPROCFS_STATS_FLAG_IRQ_SAFE) != 0)
 			percpu_cntr->lc_sum_irq -= amount;
 		else
 			percpu_cntr->lc_sum -= amount;
