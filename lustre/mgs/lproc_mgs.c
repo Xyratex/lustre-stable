929d32db703a21a280f09f7611366122d47afe7c
Jeff Mahoney <jeffm@suse.com>
2013-02-04 17:05:02 -0500

    LU-2729 mgs: fix cleanup of nonexistant MGSSELF_NAME proc file
    The mgs teardown code gets invoked in the failure case but assumes
    that the MGSSELF_NAME proc file has been created. If it hasn't been
    created, the proc code issues a WARN_ON.
    
    The setup code clears the FSDB_MGS_SELF from &fsdb->fsdb_flags when it
    creates the proc file, so we can key off of that to decide whether to
    remove it.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>
    Change-Id: Idf58d1fa572eeced5dbaa72f03e38ee46a708166
    Reviewed-on: http://review.whamcloud.com/5122
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Niu Yawei <yawei.niu@intel.com>
    Reviewed-by: James Simmons <uja.ornl@gmail.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>

--- lustre/mgs/lproc_mgs.c
+++ lustre/mgs/lproc_mgs.c
@@ -256,7 +256,9 @@ int lproc_mgs_del_live(struct obd_device *obd, struct fs_db *fsdb)
         if (!mgs->mgs_proc_live)
                 return 0;
 
-        lprocfs_remove_proc_entry(fsdb->fsdb_name, mgs->mgs_proc_live);
+	/* didn't create the proc file for MGSSELF_NAME */
+	if (!test_bit(FSDB_MGS_SELF, &fsdb->fsdb_flags))
+		lprocfs_remove_proc_entry(fsdb->fsdb_name, mgs->mgs_proc_live);
         return 0;
 }
 
