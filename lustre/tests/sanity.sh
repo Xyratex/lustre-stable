1d7ddffeda56c7ead571975460ab77913e343a6b
Lai Siyao <lai.siyao@intel.com>
2013-03-12 00:11:27 +0800

    LU-2852 script: sanity 27u should cleanup test dir
    sanity 27u should cleanup test dir before test in case there
    are remaining files from previous tests.
    
    remove 27u from ALWAYS_EXCEPT since it can pass now.
    
    Signed-off-by: Lai Siyao <lai.siyao@intel.com>
    Change-Id: Id21589c1e1ecf0f4143fad3574cd779c73adb7aa
    Reviewed-on: http://review.whamcloud.com/5670
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Bobi Jam <bobijam@gmail.com>
    Reviewed-by: Jian Yu <jian.yu@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -8,7 +8,7 @@
 set -e
 
 # bug number for skipped test: 13297 2108 9789 3637 9789 3561 12622 5188
-ALWAYS_EXCEPT="                27u   42a  42b  42c  42d  45   51d   68b   $SANITY_EXCEPT"
+ALWAYS_EXCEPT="                42a  42b  42c  42d  45   51d   68b   $SANITY_EXCEPT"
 # UPDATE THE COMMENT ABOVE WITH BUG NUMBERS WHEN CHANGING ALWAYS_EXCEPT!
 
 
@@ -1181,6 +1181,7 @@ test_27u() { # bug 4900
 #define OBD_FAIL_MDS_OSC_PRECREATE      0x139
         do_facet $SINGLEMDS lctl set_param fail_loc=0x139
         mkdir -p $DIR/$tdir
+	rm -rf $DIR/$tdir/*
         createmany -o $DIR/$tdir/t- 1000
         do_facet $SINGLEMDS lctl set_param fail_loc=0
 
