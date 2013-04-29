510f0a2278d823c14d080d1a1d69b86511c9ec27
Henri Doreau <henri.doreau@cea.fr>
2013-04-29 15:07:42 +0200

    LU-3245 mdd: Fixed OBF of the FS root
    Added a check to prevent fid access to the filesystem root directory
    from returning EINVAL.
    
    Added a sanity check accordingly.
    
    Signed-off-by: Henri Doreau <henri.doreau@cea.fr>
    Signed-off-by: John L. Hammond <john.hammond@intel.com>
    Change-Id: Iffb8bdb71377b878d165e9e48049c6b2ebd14859
    Reviewed-on: http://review.whamcloud.com/6209
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
    Tested-by: Hudson
    Reviewed-by: Fan Yong <fan.yong@intel.com>
    Tested-by: Maloo <whamcloud.maloo@gmail.com>

--- lustre/mdd/mdd_device.c
+++ lustre/mdd/mdd_device.c
@@ -904,7 +904,7 @@ static int obf_lookup(const struct lu_env *env, struct md_object *p,
                 GOTO(out, rc = -EINVAL);
         }
 
-	if (!fid_is_norm(f)) {
+	if (!fid_is_norm(f) && !fid_is_root(f)) {
 		CWARN("%s: "DFID" is invalid, sequence should be "
 		      ">= "LPX64"\n", mdd->mdd_obd_dev->obd_name, PFID(f),
 		      (__u64)FID_SEQ_NORMAL);
