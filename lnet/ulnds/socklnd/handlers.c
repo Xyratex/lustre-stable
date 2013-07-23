a69937e45a585875ca898717867eb34ed49dcc99
Vladimir Saveliev <vladimir_saveliev@xyratex.com>
2013-07-23 12:37:32 +0300

    MRP-1190 docs: update copyright headers
    Xyratex and Intel are mentioned on copyright headers
    Lustre trademark is corrected
    
    Signed-off-by: Vladimir Saveliev <vladimir_saveliev@xyratex.com>
    Change-Id: Iac11c879e620cd2ac7404d771a7c5c27405bd9e7
    Reviewed-on: http://morpheus.xyus.xyratex.com:8443/gerrit/573
    Tested-by: Jenkins
    Reviewed-by: Vitaly Fertman <Vitaly_Fertman@xyratex.com>

--- lnet/ulnds/socklnd/handlers.c
+++ lnet/ulnds/socklnd/handlers.c
@@ -1,6 +1,4 @@
-/* -*- mode: c; c-basic-offset: 8; indent-tabs-mode: nil; -*-
- * vim:expandtab:shiftwidth=8:tabstop=8:
- *
+/*
  * GPL HEADER START
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
@@ -11,27 +9,32 @@
  *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License version 2 for more details (a copy is included
  * in the LICENSE file that accompanied this code).
  *
  * You should have received a copy of the GNU General Public License
  * version 2 along with this program; If not, see
- * http://www.sun.com/software/products/lustre/docs/GPLv2.pdf
+ * http://www.lustre.org/lustre/docs/GPLv2.pdf
  *
- * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
- * CA 95054 USA or visit www.sun.com if you need additional information or
+ * Please contact Xyratex Technology, Ltd., Langstone Road, Havant, Hampshire.
+ * PO9 1SA, U.K. or visit www.xyratex.com if you need additional information or
  * have any questions.
  *
  * GPL HEADER END
  */
 /*
- * Copyright  2008 Sun Microsystems, Inc. All rights reserved
+ * Copyright (c) 2002, 2013, Xyratex Technology, Ltd . All rights reserved.
  * Use is subject to license terms.
  */
 /*
+ * Some portions of Lustre® software are subject to copyrights help by Intel Corp.
+ * Copyright (c) 2011-2013 Intel Corporation, Inc.
+ */
+/*
  * This file is part of Lustre, http://www.lustre.org/
- * Lustre is a trademark of Sun Microsystems, Inc.
+ * Lustre® and the Lustre logo are registered trademarks of
+ * Xyratex Technology, Ltd  in the United States and/or other countries.
  *
  * lnet/ulnds/socklnd/handlers.c
  *
