935c24009a287a0eee3b603eab979404418ae07f
Andriy Skulysh <Andriy_Skulysh@xyratex.com>
2013-08-20 17:41:25 +0300

    MRP-1296 ldlm: ldlm_cancel_stale_locks()) ASSERTION( count > 0 ) failed
    number of granted locks can be really huge.
    Use 64bit math to calculate number of locks to cancel
    
    Change-Id: Ia10c056a03750c242e6bb97dfad5718d54fe7e88
    Signed-off-by: Andriy Skulysh <Andriy_Skulysh@xyratex.com>
    Reviewed-on: http://morpheus.xyus.xyratex.com:8443/gerrit/637
    Reviewed-by: Alexey Lyashkov <Alexey_Lyashkov@xyratex.com>
    Tested-by: Jenkins
    Reviewed-by: Alexander Boyko <Alexander_Boyko@xyratex.com>
    Tested-by: Elena Gryaznova <Elena_Gryaznova@xyratex.com>
    Reviewed-by: Vitaly Fertman <Vitaly_Fertman@xyratex.com>

--- lustre/ldlm/ldlm_pool.c
+++ lustre/ldlm/ldlm_pool.c
@@ -1062,7 +1062,7 @@ __u32 ldlm_pool_get_lvf(struct ldlm_pool *pl)
 EXPORT_SYMBOL(ldlm_pool_get_lvf);
 
 #ifdef __KERNEL__
-static int ldlm_pool_granted(struct ldlm_pool *pl)
+static unsigned int ldlm_pool_granted(struct ldlm_pool *pl)
 {
         return cfs_atomic_read(&pl->pl_granted);
 }
@@ -1080,7 +1080,8 @@ static cfs_completion_t ldlm_pools_comp;
 static int ldlm_pools_shrink(ldlm_side_t client, int nr,
                              unsigned int gfp_mask)
 {
-        int total = 0, cached = 0, nr_ns;
+	unsigned int total = 0, cached = 0;
+	int nr_ns;
         struct ldlm_namespace *ns;
         void *cookie;
 
@@ -1124,7 +1125,8 @@ static int ldlm_pools_shrink(ldlm_side_t client, int nr,
         for (nr_ns = cfs_atomic_read(ldlm_namespace_nr(client));
              nr_ns > 0; nr_ns--)
         {
-                int cancel, nr_locks;
+		__u64 cancel;
+		unsigned int nr_locks;
 
                 /*
                  * Do not call shrink under ldlm_namespace_lock(client)
@@ -1146,8 +1148,9 @@ static int ldlm_pools_shrink(ldlm_side_t client, int nr,
                 cfs_mutex_unlock(ldlm_namespace_lock(client));
 
                 nr_locks = ldlm_pool_granted(&ns->ns_pool);
-                cancel = 1 + nr_locks * nr / total;
-                ldlm_pool_shrink(&ns->ns_pool, cancel, gfp_mask);
+		cancel = (__u64)nr_locks * nr;
+		do_div(cancel, total);
+		ldlm_pool_shrink(&ns->ns_pool, 1 + cancel, gfp_mask);
                 cached += ldlm_pool_granted(&ns->ns_pool);
                 ldlm_namespace_put(ns);
         }
