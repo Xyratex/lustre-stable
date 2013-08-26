eba0d425977434974096d1e930d8dd369984a75c
Alexey Lyashkov <alexey_lyashkov@xyratex.com>
2013-08-26 07:51:38 +0300

    MRP-1306 target: Fix assigning an connection flag for export.
    Change-Id: I68d81536e2506bb9cf56e6bb7ce039313eb39426
    Signed-off-by: Alexey Lyashkov <alexey_lyashkov@xyratex.com>
    Reviewed-on: http://morpheus.xyus.xyratex.com:8443/gerrit/647
    Tested-by: Jenkins
    Reviewed-by: Alexander Boyko <Alexander_Boyko@xyratex.com>
    Reviewed-by: Andrew Perepechko <Andrew_Perepechko@xyratex.com>
    Reviewed-by: Sergey Cheremencev <Sergey_Cheremencev@xyratex.com>
    Tested-by: Elena Gryaznova <Elena_Gryaznova@xyratex.com>
    Reviewed-by: Vitaly Fertman <Vitaly_Fertman@xyratex.com>
    Signed-off-by: Sergey Cheremencev <Sergey_Cheremencev@xyratex.com>
    Reviewed-on: http://morpheus.xyus.xyratex.com:8443/gerrit/660
    Reviewed-by: Alexey Lyashkov <Alexey_Lyashkov@xyratex.com>

--- lustre/ost/ost_handler.c
+++ lustre/ost/ost_handler.c
@@ -1571,14 +1571,11 @@ static int ost_llog_handle_connect(struct obd_export *exp,
         RETURN(rc);
 }
 
-#define ost_init_sec_none(reply, exp)                                   \
+#define ost_init_sec_none(reply)                                        \
 do {                                                                    \
         reply->ocd_connect_flags &= ~(OBD_CONNECT_RMT_CLIENT |          \
                                       OBD_CONNECT_RMT_CLIENT_FORCE |    \
                                       OBD_CONNECT_OSS_CAPA);            \
-        cfs_spin_lock(&exp->exp_lock);                                  \
-	*exp_connect_flags_ptr(exp) = reply->ocd_connect_flags;		\
-        cfs_spin_unlock(&exp->exp_lock);                                \
 } while (0)
 
 static int ost_init_sec_level(struct ptlrpc_request *req)
@@ -1599,7 +1596,7 @@ static int ost_init_sec_level(struct ptlrpc_request *req)
 
         /* connection from MDT is always trusted */
         if (req->rq_auth_usr_mdt) {
-                ost_init_sec_none(reply, exp);
+                ost_init_sec_none(reply);
                 RETURN(0);
         }
 
@@ -1611,7 +1608,7 @@ static int ost_init_sec_level(struct ptlrpc_request *req)
                               client, obd->obd_name, filter->fo_sec_level);
                         RETURN(-EACCES);
                 } else {
-                        ost_init_sec_none(reply, exp);
+                        ost_init_sec_none(reply);
                         RETURN(0);
                 }
         }
@@ -1628,7 +1625,7 @@ static int ost_init_sec_level(struct ptlrpc_request *req)
                         CWARN("client %s -> target %s uses old version, "
                               "run under security level %d.\n",
                               client, obd->obd_name, filter->fo_sec_level);
-                        ost_init_sec_none(reply, exp);
+                        ost_init_sec_none(reply);
                         RETURN(0);
                 }
         }
@@ -1656,7 +1653,7 @@ static int ost_init_sec_level(struct ptlrpc_request *req)
         switch (filter->fo_sec_level) {
         case LUSTRE_SEC_NONE:
                 if (!remote) {
-                        ost_init_sec_none(reply, exp);
+                        ost_init_sec_none(reply);
                         break;
                 } else {
                         CDEBUG(D_SEC, "client %s -> target %s is set as remote, "
@@ -1666,7 +1663,7 @@ static int ost_init_sec_level(struct ptlrpc_request *req)
                 }
         case LUSTRE_SEC_REMOTE:
                 if (!remote)
-                        ost_init_sec_none(reply, exp);
+                        ost_init_sec_none(reply);
                 break;
         case LUSTRE_SEC_ALL:
                 if (!remote) {
@@ -1674,10 +1671,6 @@ static int ost_init_sec_level(struct ptlrpc_request *req)
                                                       OBD_CONNECT_RMT_CLIENT_FORCE);
                         if (!filter->fo_fl_oss_capa)
                                 reply->ocd_connect_flags &= ~OBD_CONNECT_OSS_CAPA;
-
-                        cfs_spin_lock(&exp->exp_lock);
-                        *exp_connect_flags_ptr(exp) = reply->ocd_connect_flags;
-                        cfs_spin_unlock(&exp->exp_lock);
                 }
                 break;
         default:
@@ -2354,7 +2347,19 @@ int ost_handle(struct ptlrpc_request *req)
                         rc = ost_init_sec_level(req);
                         if (!rc)
                                 rc = ost_connect_check_sptlrpc(req);
-                }
+		}
+		if (rc == 0) {
+			struct obd_export *exp = req->rq_export;
+			struct obd_connect_data *reply;
+			/* To avoid exposing partially initialized connection flags, changes up
+			 * to this point have been staged in reply->ocd_connect_flags. Now that
+			 * connection handling has completed successfully, atomically update
+			 * the connect flags in the shared export data structure. LU-1623 */
+			reply = req_capsule_server_get(&req->rq_pill, &RMF_CONNECT_DATA);
+			spin_lock(&exp->exp_lock);
+			*exp_connect_flags_ptr(exp) = reply->ocd_connect_flags;
+			spin_unlock(&exp->exp_lock);
+		}
                 break;
         }
         case OST_DISCONNECT:
