d8df52d538ea362e2690d9b04d97039248c4899d
Liang Zhen <liang.zhen@intel.com>
2013-09-15 23:09:53 +0800

    LU-3055 libcfs: remove page_collection::pc_lock
    page_collection::pc_lock is supposed to protect race between
    functions called by smp_call_function(), however we don't have
    this use-case for ages and page_collection only lives in stack
    of thread, so it is safe to remove it.
    
    Signed-off-by: Liang Zhen <liang.zhen@intel.com>
    Change-Id: I89ca46e6593838d85fc0b91d1ff7c35cc28cf4ce
    Reviewed-on: http://review.whamcloud.com/7660
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Bobi Jam <bobijam@gmail.com>
    Reviewed-by: Sebastien Buisson <sebastien.buisson@bull.net>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- libcfs/libcfs/tracefile.c
+++ libcfs/libcfs/tracefile.c
@@ -199,7 +199,6 @@ static void cfs_tcd_shrink(struct cfs_trace_cpu_data *tcd)
                        pgcount + 1, tcd->tcd_cur_pages);
 
         CFS_INIT_LIST_HEAD(&pc.pc_pages);
-        cfs_spin_lock_init(&pc.pc_lock);
 
         cfs_list_for_each_entry_safe_typed(tage, tmp, &tcd->tcd_pages,
                                            struct cfs_trace_page, linkage) {
@@ -527,7 +526,6 @@ static void collect_pages_on_all_cpus(struct page_collection *pc)
         struct cfs_trace_cpu_data *tcd;
         int i, cpu;
 
-        cfs_spin_lock(&pc->pc_lock);
         cfs_for_each_possible_cpu(cpu) {
                 cfs_tcd_for_each_type_lock(tcd, i, cpu) {
                         cfs_list_splice_init(&tcd->tcd_pages, &pc->pc_pages);
@@ -539,7 +537,6 @@ static void collect_pages_on_all_cpus(struct page_collection *pc)
                         }
                 }
         }
-        cfs_spin_unlock(&pc->pc_lock);
 }
 
 static void collect_pages(struct page_collection *pc)
@@ -560,7 +557,6 @@ static void put_pages_back_on_all_cpus(struct page_collection *pc)
         struct cfs_trace_page *tmp;
         int i, cpu;
 
-        cfs_spin_lock(&pc->pc_lock);
         cfs_for_each_possible_cpu(cpu) {
                 cfs_tcd_for_each_type_lock(tcd, i, cpu) {
                         cur_head = tcd->tcd_pages.next;
@@ -580,7 +576,6 @@ static void put_pages_back_on_all_cpus(struct page_collection *pc)
                         }
                 }
         }
-        cfs_spin_unlock(&pc->pc_lock);
 }
 
 static void put_pages_back(struct page_collection *pc)
@@ -599,7 +594,6 @@ static void put_pages_on_tcd_daemon_list(struct page_collection *pc,
         struct cfs_trace_page *tage;
         struct cfs_trace_page *tmp;
 
-        cfs_spin_lock(&pc->pc_lock);
         cfs_list_for_each_entry_safe_typed(tage, tmp, &pc->pc_pages,
                                            struct cfs_trace_page, linkage) {
 
@@ -624,7 +618,6 @@ static void put_pages_on_tcd_daemon_list(struct page_collection *pc,
                         tcd->tcd_cur_daemon_pages--;
                 }
         }
-        cfs_spin_unlock(&pc->pc_lock);
 }
 
 static void put_pages_on_daemon_list(struct page_collection *pc)
@@ -643,8 +636,6 @@ void cfs_trace_debug_print(void)
         struct page_collection pc;
         struct cfs_trace_page *tage;
         struct cfs_trace_page *tmp;
-
-        cfs_spin_lock_init(&pc.pc_lock);
 
         pc.pc_want_daemon_pages = 1;
         collect_pages(&pc);
@@ -700,7 +691,6 @@ int cfs_tracefile_dump_all_pages(char *filename)
                 goto out;
         }
 
-        cfs_spin_lock_init(&pc.pc_lock);
         pc.pc_want_daemon_pages = 1;
         collect_pages(&pc);
         if (cfs_list_empty(&pc.pc_pages)) {
@@ -744,8 +734,6 @@ void cfs_trace_flush_pages(void)
         struct page_collection pc;
         struct cfs_trace_page *tage;
         struct cfs_trace_page *tmp;
-
-        cfs_spin_lock_init(&pc.pc_lock);
 
         pc.pc_want_daemon_pages = 1;
         collect_pages(&pc);
@@ -992,7 +980,6 @@ static int tracefiled(void *arg)
         /* this is so broken in uml?  what on earth is going on? */
         cfs_daemonize("ktracefiled");
 
-        cfs_spin_lock_init(&pc.pc_lock);
         cfs_complete(&tctl->tctl_start);
 
         while (1) {
@@ -1191,7 +1178,6 @@ static void cfs_trace_cleanup(void)
         struct page_collection pc;
 
         CFS_INIT_LIST_HEAD(&pc.pc_pages);
-        cfs_spin_lock_init(&pc.pc_lock);
 
         trace_cleanup_on_all_cpus();
 
