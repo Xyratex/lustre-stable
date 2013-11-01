16a84b71fd48b3c2f2b5b55a726ce69ed057e5c3
James Nunez <james.a.nunez@intel.com>
2013-11-01 12:18:05 -0600

    LU-3867 utils: llapi_create_volatile_idx uses 0 instead idx
    If llapi_create_volatile_idx() is passed an MDT index other than
    -1, it always uses 0 as the MDT index rather than the one provided.
    
    This patch removes the use of LUSTRE_VOLATILE_IDX and uses
    LUSTRE_VOLATILE_HDR with the MDT index instead, uses the provided
    MDT index in the volatile file name and uses base 16 when reading
    the MDT index from the file name using strtoul.
    
    Signed-off-by: James Nunez <james.a.nunez@intel.com>
    Signed-off-by: Bobbie Lind <bobbie.j.lind@intel.com>
    Change-Id: I27e2beb8524683aba07c48297614dd02d7957af4
    Reviewed-on: http://review.whamcloud.com/7613
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>

--- lustre/include/obd.h
+++ lustre/include/obd.h
@@ -1602,7 +1602,7 @@ static inline bool filename_is_volatile(const char *name, int namelen, int *idx)
 	}
 	/* we have an idx, read it */
 	start = name + LUSTRE_VOLATILE_HDR_LEN + 1;
-	*idx = strtoul(start, &end, 0);
+	*idx = strtoul(start, &end, 16);
 	/* error cases:
 	 * no digit, no trailing :, negative value
 	 */
