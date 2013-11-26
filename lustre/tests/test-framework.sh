bd95c055658a49caad3944059429ceab968e171d
Elena Gryaznova <Elena_Gryaznova@xyratex.com>
2013-11-26 14:38:29 +0400

    MRP-1486 tests: fix assign target index
    wait until target will registed on mgs to obtain correct index
    in e2label result
    
    Signed-off-by: Elena Gryaznova <Elena_Gryaznova@xyratex.com>
    Change-Id: I8e6b7b90d0001e7302c97970fedcba5f0a2b2c73
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/975
    Tested-by: Jenkins
    Reviewed-by: Elena Gryaznova <elena_gryaznova@xyratex.com>
    Tested-by: Elena Gryaznova <elena_gryaznova@xyratex.com>
    Reviewed-by: Vitaly Fertman <vitaly_fertman@xyratex.com>

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -637,17 +637,23 @@ facet_fstype() {
     echo -n $FSTYPE
 }
 
-mdsdevlabel() {
-    local num=$1
-    local device=`mdsdevname $num`
-    local label=`do_facet mds$num "e2label ${device}" | grep -v "CMD: "`
-    echo -n $label
-}
 
-ostdevlabel() {
-    local num=$1
-    local device=`ostdevname $num`
-    local label=`do_facet ost$num "e2label ${device}" | grep -v "CMD: "`
+facetlabel() {
+    local name=$1
+    local device=$2
+    local label
+    local i
+
+    # 2*timeout is enough to have reconnect until target is registered
+    for (( i = 0; $i <= $(($TIMEOUT * 2)); i++ )); do
+	label=$(do_facet $name "$E2LABEL ${device}")
+        # ffff means that index is not assigned
+	if [[ "$label" != *ffff ]]; then
+	    break;
+	fi
+	label=""
+	sleep 1
+    done
     echo -n $label
 }
 
@@ -758,7 +764,7 @@ mount_facet() {
     else
         set_default_debug_facet $facet
 
-        label=$(do_facet ${facet} "$E2LABEL ${!dev}")
+        label=$(facetlabel ${facet} ${!dev})
         [ -z "$label" ] && echo no label for ${!dev} && exit 1
         eval export ${facet}_svc=${label}
         echo Started ${label}
@@ -2594,7 +2600,7 @@ init_facet_vars () {
     eval export ${facet}_opt=\"$@\"
 
     local dev=${facet}_dev
-    local label=$(do_facet ${facet} "$E2LABEL ${!dev}")
+    local label=$(facetlabel ${facet} ${!dev})
     [ -z "$label" ] && echo no label for ${!dev} && exit 1
 
     eval export ${facet}_svc=${label}
