811bb9bfdab43cebce5138ee8b6dc7ba6d0fd41d
Bruno Faccini <bruno.faccini@intel.com>
2013-11-26 11:46:07 +0100

    LU-4279 mdd: Fixed OBF of the FS .lustre
    Added a check to prevent fid access to the filesystem .lustre
    directory from returning EINVAL.
    Added a sanity check accordingly.
    
    Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>
    Change-Id: I3002e8dd1b834a0b6b87fe707e12573c2accb58c
    Reviewed-on: http://review.whamcloud.com/8360
    Reviewed-by: Fan Yong <fan.yong@intel.com>
    Reviewed-by: Sebastien Buisson <sebastien.buisson@bull.net>
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/mdd/mdd_device.c
+++ lustre/mdd/mdd_device.c
@@ -904,7 +904,8 @@ static int obf_lookup(const struct lu_env *env, struct md_object *p,
                 GOTO(out, rc = -EINVAL);
         }
 
-	if (!fid_is_norm(f) && !fid_is_root(f)) {
+	if (!fid_is_norm(f) && !fid_is_root(f) &&
+	    !fid_seq_is_dot(f->f_seq)) {
 		CWARN("%s: "DFID" is invalid, sequence should be "
 		      ">= "LPX64"\n", mdd->mdd_obd_dev->obd_name, PFID(f),
 		      (__u64)FID_SEQ_NORMAL);
