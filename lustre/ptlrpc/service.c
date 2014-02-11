sdsc/lustre-release

LU-4578 ptlrpc: Early replies need to honor at_max
When determining whether an early reply can be sent the server will
calculate the new deadline based on an offset from the request
arrival time. However, when actually setting the new deadline
the server offsets the current time. This can result in deadlines
being extended more than at_max seconds past the request arrival
time.

Signed-off-by: Chris Horn <hornc@cray.com>
Change-Id: I1b7b04bd20ea5d471ab3777f987e89add744b813

lustre/ptlrpc/service.c
@@ -1347,7 +1347,7 @@ static int ptlrpc_at_send_early_reply(struct ptlrpc_request *req)
 			RETURN(-ETIMEDOUT);
 		}
 	}
-	newdl = cfs_time_current_sec() + at_get(&svcpt->scp_at_estimate);
+	newdl = req->rq_arrival_time.tv_sec + at_get(&svcpt->scp_at_estimate);
 
 	reqcopy = ptlrpc_request_cache_alloc(__GFP_IO);
 	if (reqcopy == NULL)

Xyratex/lustre-stable

Change-Id: I73e92e36d6bb806af714a41715dcf0c3661cb47c

lustre/ptlrpc/service.c
@@ -1136,7 +1136,7 @@ static int ptlrpc_at_send_early_reply(struct ptlrpc_request *req)
                         RETURN(-ETIMEDOUT);
                 }
         }
-        newdl = cfs_time_current_sec() + at_get(&svc->srv_at_estimate);
+        newdl = req->rq_arrival_time.tv_sec + at_get(&svc->srv_at_estimate);
 
 	reqcopy = ptlrpc_request_alloc_cache(CFS_ALLOC_IO);
         if (reqcopy == NULL)