486e96529d69fdf7b96caa41368cbe9a9b6432bd
Henri Doreau <henri.doreau@cea.fr>
2014-04-18 21:47:37 +0200

    LU-4928 utils: LLAPI helpers for file leases
    Introduced llapi_lease_{get,check,put} functions to abstract the ioctl
    implementation to manipulate file leases.
    
    Signed-off-by: Henri Doreau <henri.doreau@cea.fr>
    Change-Id: I956408defe550c1b432526e142825ea736d0e285
    Reviewed-on: http://review.whamcloud.com/10022
    Tested-by: Jenkins
    Reviewed-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>

--- lustre/include/lustre/lustreapi.h
+++ lustre/include/lustre/lustreapi.h
@@ -361,9 +361,12 @@ extern int llapi_json_add_item(struct llapi_json_item_list **item_list,
 			       char *key, __u32 type, void *val);
 extern int llapi_json_write_list(struct llapi_json_item_list **item_list,
 				 FILE *fp);
+/* File lease */
+extern int llapi_lease_get(int fd, int mode);
+extern int llapi_lease_check(int fd);
+extern int llapi_lease_put(int fd);
+
 /** @} llapi */
 
 #endif
-
-
 
