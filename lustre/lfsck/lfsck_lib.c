2070eca8f5579a6851c95fc4f3d69b520a7d1839
Ryan Haasken <haasken@cray.com>
2014-05-08 09:18:36 -0500

    LU-4985 kernel: Store kthread_run result in task_struct ptr
    The fix for LU-3498 changed most invocations of kthread_run to store
    the result in a variable of task_struct pointer type and check whether
    kthread_run returned an error using IS_ERR.  This change updates the
    remaining calls to kthread_run to do this as well for consistency.
    
    Signed-off-by: Ryan Haasken <haasken@cray.com>
    Change-Id: I18edab018080ec51e9979164234e5bcd02a04f88
    Reviewed-on: http://review.whamcloud.com/10266
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: James Simmons <uja.ornl@gmail.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>

--- lustre/lfsck/lfsck_lib.c
+++ lustre/lfsck/lfsck_lib.c
@@ -858,7 +858,8 @@ int lfsck_start(const struct lu_env *env, struct dt_device *key,
 	struct lfsck_component *com;
 	struct l_wait_info      lwi    = { 0 };
 	bool			dirty  = false;
-	long			rc     = 0;
+	struct task_struct     *task;
+	int			rc     = 0;
 	__u16			valid  = 0;
 	__u16			flags  = 0;
 	ENTRY;
@@ -1009,9 +1010,10 @@ trigger:
 
 	lfsck->li_args_oit = (flags << DT_OTABLE_IT_FLAGS_SHIFT) | valid;
 	thread_set_flags(thread, 0);
-	rc = PTR_ERR(kthread_run(lfsck_master_engine, lfsck, "lfsck"));
-	if (IS_ERR_VALUE(rc)) {
-		CERROR("%s: cannot start LFSCK thread, rc = %ld\n",
+	task = kthread_run(lfsck_master_engine, lfsck, "lfsck");
+	if (IS_ERR(task)) {
+		rc = PTR_ERR(task);
+		CERROR("%s: cannot start LFSCK thread: rc = %d\n",
 		       lfsck_lfsck2name(lfsck), rc);
 	} else {
 		rc = 0;
