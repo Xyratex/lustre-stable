143defffffc9f6b06419680444c9ea8b6c18c00e
Andriy Skulysh <Andriy_Skulysh@xyratex.com>
2014-06-16 17:58:45 +0400

    MRP-1944 ldlm: ldlm_process_extent_lock()) ASSERTION failed
    ASSERTION( lock->l_granted_mode != lock->l_req_mode ) is failing
    when resending LDLM_FL_REPLAY with LDLM_FL_BLOCK_WAIT.
    
    Don't add lock to waiting list as it is already added or granted.
    
    Change-Id: Ib8e5d2c7588f6cacd1723529e70d29f63742caad
    Signed-off-by: Andriy Skulysh <Andriy_Skulysh@xyratex.com>
    Reviewed-by: Vitaly Fertman <vitaly_fertman@xyratex.com>
    Reviewed-by: Alexander Boyko <alexander_boyko@xyratex.com>

--- lustre/ldlm/ldlm_lock.c
+++ lustre/ldlm/ldlm_lock.c
@@ -1405,6 +1405,8 @@ ldlm_error_t ldlm_lock_enqueue(struct ldlm_namespace *ns,
                 else
                         ldlm_grant_lock(lock, NULL);
                 GOTO(out, ELDLM_OK);
+	} else if(*flags & LDLM_FL_RESENT) {
+		GOTO(out, rc = ELDLM_OK);
         } else if (*flags & LDLM_FL_REPLAY) {
                 if (*flags & LDLM_FL_BLOCK_CONV) {
                         ldlm_resource_add_lock(res, &res->lr_converting, lock);
