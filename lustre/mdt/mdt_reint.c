a3a60c66e5660b2cd5c06c279d8add6b09af35cd
James Simmons <uja.ornl@gmail.com>
2014-08-18 10:37:13 -0400

    LU-3963 libcfs: convert lod, mdt, and gss to linux list api
    Move from the cfs_[h]list api to the native linux api for
    the lod, mdt, and gss part of the ptlrpc layers.
    
    Change-Id: Ieff231f3220a850521713a6f1c997b7e09130a4c
    Signed-off-by: James Simmons <uja.ornl@gmail.com>
    Reviewed-on: http://review.whamcloud.com/10387
    Reviewed-by: Bob Glossman <bob.glossman@intel.com>
    Tested-by: Jenkins
    Reviewed-by: frank zago <fzago@cray.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/mdt/mdt_reint.c
+++ lustre/mdt/mdt_reint.c
@@ -550,7 +550,7 @@ static int mdt_reint_setattr(struct mdt_thread_info *info,
                 mfd->mfd_xid = req->rq_xid;
 
 		spin_lock(&med->med_open_lock);
-		cfs_list_add(&mfd->mfd_list, &med->med_open_head);
+		list_add(&mfd->mfd_list, &med->med_open_head);
 		spin_unlock(&med->med_open_lock);
 		repbody->mbo_handle.cookie = mfd->mfd_handle.h_cookie;
         }
@@ -576,8 +576,8 @@ static int mdt_reint_setattr(struct mdt_thread_info *info,
                 LASSERT(mfd->mfd_mode == MDS_FMODE_SOM);
                 LASSERT(!(info->mti_ioepoch->flags & MF_EPOCH_CLOSE));
 
-                class_handle_unhash(&mfd->mfd_handle);
-                cfs_list_del_init(&mfd->mfd_list);
+		class_handle_unhash(&mfd->mfd_handle);
+		list_del_init(&mfd->mfd_list);
 		spin_unlock(&med->med_open_lock);
 
                 mdt_mfd_close(info, mfd);
