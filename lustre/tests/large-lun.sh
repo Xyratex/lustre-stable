250acc0377e3c88f3818358aba3e9b08a8e87e69
Elena Gryaznova <elena_gryaznova@xyratex.com>
2014-08-28 18:27:38 +0400

    MRP-1984 tests: fix large-lun test_1, test_2 defects
    setupall()->start ost called at the end of large-lun.sh
    fails to start the ost devices which leads test_1() and
    test_2() fail if run separately.
    The reasons are:
    1) osts data are destroyed by test_1()->llverdev
    2) osts are reformatted by test_2, but mds config not changed
    Patch fixes the following:
    - format lustre at the end of test_1() and test_2()
    - add the "REFORMAT" option check to avoid killing
    filesystems that do not want a reformat.
    
    Signed-off-by: Elena Gryaznova <Elena_Gryaznova@xyratex.com>
    Change-Id: Ifaeea3e4b3008ae6a72e5e4dc6923840f12437ff
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/3311
    Reviewed-by: Vladimir Saveliev <vladimir_saveliev@xyratex.com>
    Tested-by: Jenkins
    Tested-by: Elena Gryaznova <elena_gryaznova@xyratex.com>
    Reviewed-by: Vitaly Fertman <vitaly_fertman@xyratex.com>

--- lustre/tests/large-lun.sh
+++ lustre/tests/large-lun.sh
@@ -11,6 +11,11 @@ init_test_env $@
 . ${CONFIG:=$LUSTRE/tests/cfg/$NAME.sh}
 init_logging
 
+if ! [ "$REFORMAT" ]; then
+	skip_env "$0 reformats all devices, please set REFORMAT to run this test"
+	exit 0
+fi
+
 # Variable to run mdsrate
 THREADS_PER_CLIENT=${THREADS_PER_CLIENT:-5}    # thread(s) per client node
 MACHINEFILE=${MACHINEFILE:-$TMP/$TESTSUITE.machines}
@@ -120,6 +125,8 @@ test_1 () {
 		do_rpc_nodes $(facet_host ost${num}) run_llverdev $dev -vpf ||
 			error "llverdev on $dev failed!"
 	done
+	# restore format overwritten by llverdev
+	formatall
 }
 run_test 1 "run llverdev on raw LUN"
 
@@ -184,6 +191,9 @@ test_2 () {
 				"-y" || error "run e2fsck error"
 		fi
 	done
+	# there is no reason to continue using ost devices
+	# filled by llverfs as ldiskfs
+	formatall
 }
 run_test 2 "run llverfs on OST ldiskfs filesystem"
 
