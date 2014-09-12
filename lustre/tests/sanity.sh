8b2eb3e1d6bffaa2489cc9d48a3be0a12f77373d
Vitaly Fertman <vitaly.fertman@seagate.com>
2014-09-12 22:58:14 +0400

    LU-3192 osc: split different type of IO
    Do not allow different type of pages at the same rpc.
    
    Signed-off-by: Alexander Boyko <alexander_boyko@xyratex.com>
    Signed-off-by: Vitaly Fertman <vitaly_fertman@xyratex.com>
    Xyratex-bug-id: MRP-859
    Change-Id: Ic595a29f685757faf4d9c3de9c2d2ae8fd039baf
    Reviewed-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Reviewed-by: Niu Yawei <yawei.niu@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -12180,6 +12180,31 @@ test_238() {
 }
 run_test 238 "Verify linkea consistency"
 
+test_241_bio() {
+	for LOOP in $(seq $1); do
+		dd if=$DIR/$tfile of=/dev/null bs=40960 count=1 2>/dev/null
+		cancel_lru_locks osc > /dev/null
+	done
+}
+
+test_241_dio() {
+	for LOOP in $(seq $1); do
+		dd if=$DIR/$tfile of=/dev/null bs=40960 count=1 \
+						iflag=direct 2>/dev/null
+	done
+}
+
+test_241() {
+	dd if=/dev/zero of=$DIR/$tfile count=1 bs=40960
+	ls -la $DIR/$tfile
+	cancel_lru_locks osc
+	test_241_bio 1000 &
+	PID=$!
+	test_241_dio 1000
+	wait $PID
+}
+run_test 241 "bio vs dio"
+
 #
 # tests that do cleanup/setup should be run at the end
 #
