8335f0dec8159085b154aeb6e7adb3e0d15910fd
Vladimir Saveliev <vladimir.saveliev@seagate.com>
2014-10-01 10:43:49 +0400

    MRP-2105 mgs: fix mgs_backup_llog
    -read source/write target loop in mgs_backup_llog is corrected
    -writes to llog file go via buffer cache and transactions
     (fsfilt_ldiskfs_write_handle), so before reading the llog file via
     page cache based file read the transactions which wrote the llog file
     are to be flushed (checkpointed, not only committed).
     fs_journal_flush operation is added to struct fsfilt_operations and
     is called before accessing a llog file via file read.
    
    Change-Id: I71dbf6efb9c115587162da6cd0b628674551a803
    Signed-off-by: Vladimir Saveliev <vladimir.saveliev@seagate.com>
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/3860
    Reviewed-by: Alexander Boyko <alexander_boyko@xyratex.com>
    Reviewed-by: Artem Blagodarenko <artem_blagodarenko@xyratex.com>
    Tested-by: Elena Gryaznova <elena.gryaznova@seagate.com>
    Reviewed-by: Vitaly Fertman <vitaly.fertman@seagate.com>

--- lustre/include/linux/lustre_fsfilt.h
+++ lustre/include/linux/lustre_fsfilt.h
@@ -79,6 +79,7 @@ struct fsfilt_operations {
         int     (* fs_commit_async)(struct inode *inode, void *handle,
                                         void **wait_handle);
         int     (* fs_commit_wait)(struct inode *inode, void *handle);
+        int     (* fs_journal_flush)(struct inode *inode);
         int     (* fs_setattr)(struct dentry *dentry, void *handle,
                                struct iattr *iattr, int do_trunc);
         int     (* fs_iocontrol)(struct inode *inode, struct file *file,
@@ -318,6 +319,18 @@ static inline int fsfilt_commit_wait(struct obd_device *obd,
         int rc = obd->obd_fsops->fs_commit_wait(inode, handle);
         CDEBUG(D_INFO, "waiting for completion %p\n", handle);
         fsfilt_check_slow(obd, now, "journal start");
+        return rc;
+}
+
+static inline int fsfilt_journal_flush(struct obd_device *obd,
+                                       struct inode *inode)
+{
+        unsigned long now = jiffies;
+        int rc;
+
+        CDEBUG(D_INFO, "waiting for journal flush\n");
+        rc = obd->obd_fsops->fs_journal_flush(inode);
+        fsfilt_check_slow(obd, now, "journal flush");
         return rc;
 }
 
--- lustre/lvfs/fsfilt_ext3.c
+++ lustre/lvfs/fsfilt_ext3.c
@@ -109,6 +109,7 @@ extern int ext3_xattr_set_handle(handle_t *, struct inode *, int, const char *, 
 
 #define fsfilt_log_start_commit(journal, tid) jbd2_log_start_commit(journal, tid)
 #define fsfilt_log_wait_commit(journal, tid) jbd2_log_wait_commit(journal, tid)
+#define fsfilt_log_journal_flush(journal) jbd2_journal_flush(journal)
 
 #ifdef HAVE_EXT4_JOURNAL_CALLBACK_ADD
 # define journal_callback ext4_journal_cb_entry
@@ -551,6 +552,13 @@ static int fsfilt_ext3_commit_wait(struct inode *inode, void *h)
         if (unlikely(is_journal_aborted(journal)))
                 return -EIO;
         return 0;
+}
+
+static int fsfilt_ext3_journal_flush(struct inode *inode)
+{
+        CDEBUG(D_INODE, "journal flush\n");
+
+        return fsfilt_log_journal_flush(EXT3_JOURNAL(inode));
 }
 
 static int fsfilt_ext3_setattr(struct dentry *dentry, void *handle,
@@ -2310,6 +2318,7 @@ static struct fsfilt_operations fsfilt_ext3_ops = {
         .fs_commit              = fsfilt_ext3_commit,
         .fs_commit_async        = fsfilt_ext3_commit_async,
         .fs_commit_wait         = fsfilt_ext3_commit_wait,
+        .fs_journal_flush       = fsfilt_ext3_journal_flush,
         .fs_setattr             = fsfilt_ext3_setattr,
         .fs_iocontrol           = fsfilt_ext3_iocontrol,
         .fs_set_md              = fsfilt_ext3_set_md,
