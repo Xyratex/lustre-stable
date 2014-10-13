56f3ea3ae2e23342749b606a8b71708d58222d0c
Vladimir Saveliev <vladimir.saveliev@seagate.com>
2014-10-13 14:29:23 +0400

    MRP-2091 mgs: remove skip records from config file
    Configuration logs are append only files of limited size. In course of
    time the logs may grow over the limit size. Usually, configuration
    logs keep needless records marked as SKIP. The new lctl command
    "clear_conf" is added to allow administartors to clear configuration
    files by removing mentioned SKIP records.
    lctl man page is updated.
    conf-sanity test is added to test the new command.
    
    Change-Id: Iee71234442c7e2fcc289955d11e055e2e86377a4
    Signed-off-by: Vladimir Saveliev <vladimir.saveliev@seagate.com>
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/4003
    Reviewed-by: Artem Blagodarenko <artem_blagodarenko@xyratex.com>
    Tested-by: Jenkins
    Reviewed-by: Alexey Lyashkov <alexey_lyashkov@xyratex.com>
    Tested-by: Elena Gryaznova <elena.gryaznova@seagate.com>
    Reviewed-by: Vitaly Fertman <vitaly.fertman@seagate.com>

--- lustre/mgs/mgs_handler.c
+++ lustre/mgs/mgs_handler.c
@@ -1162,7 +1162,7 @@ out_free:
                         RETURN(-EINVAL);
                 }
 
-                if (data->ioc_plen1 > MTI_NAME_MAXLEN) {
+                if (data->ioc_inllen1 > MTI_NAME_MAXLEN) {
                         CERROR("Device name is too long\n");
                         RETURN(-EOVERFLOW);
                 }
@@ -1182,6 +1182,29 @@ out_free:
                 if (rc) {
                         CERROR("error %d replacing nids\n", rc);
                 }
+                RETURN(rc);
+        }
+
+        case OBD_IOC_CLEAR_CONFIGS: {
+                if (!data->ioc_inllen1 || !data->ioc_inlbuf1) {
+                        CERROR("No device or fs name specified!\n");
+                        RETURN(-EINVAL);
+                }
+
+                if (data->ioc_inlbuf1[data->ioc_inllen1 - 1] != 0) {
+                        CERROR("Device or fs name is not NUL terminated!\n");
+                        RETURN(-EINVAL);
+                }
+
+                if (data->ioc_inllen1 > MTI_NAME_MAXLEN) {
+                        CERROR("Device or fs name is too long\n");
+                        RETURN(-EOVERFLOW);
+                }
+
+                /* remove records marked SKIP from config logs */
+                rc = mgs_clear_configs(obd, data->ioc_inlbuf1);
+                if (rc)
+                        CERROR("error %d clearing config log\n", rc);
                 RETURN(rc);
         }
 
