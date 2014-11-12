4a35eed8279f1880c9edd067e7d32b0eff716b80
Vladimir Saveliev <vladimir.saveliev@seagate.com>
2014-11-12 15:40:17 +0400

    MRP-2021 mdd: credits for changelog calculation
    Changelog record addition may require object creation and rename, and
    writing 4 records each up to 2 blocks long, while only 16 credits are
    reserved (or 14 when quota is off).
    Reserve space for the operation so that it was enough for all the
    suboperations.
    
    Change-Id: I09f9722e7b77121fb0c4b736d2677742fdfdb7db
    Signed-off-by: Vladimir Saveliev <vladimir.saveliev@seagate.com>
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/4371
    Tested-by: Jenkins
    Reviewed-by: Alexander Boyko <alexander_boyko@xyratex.com>
    Reviewed-by: Alexey Lyashkov <alexey_lyashkov@xyratex.com>
    Tested-by: Elena Gryaznova <elena.gryaznova@seagate.com>
    Reviewed-by: Vitaly Fertman <vitaly.fertman@seagate.com>

--- lustre/mdd/mdd_trans.c
+++ lustre/mdd/mdd_trans.c
@@ -104,8 +104,10 @@ void mdd_txn_param_build(const struct lu_env *env, struct mdd_device *mdd,
         txn_param_init(&mdd_env_info(env)->mti_param,
                        mdd->mdd_tod[op].mod_credits);
         if (changelog_cnt > 0) {
-                txn_param_credit_add(&mdd_env_info(env)->mti_param,
-                                  changelog_cnt * dto_txn_credits[DTO_LOG_REC]);
+		op = MDD_TXN_CHANGELOG_OP;
+		txn_param_credit_add(&mdd_env_info(env)->mti_param,
+				     changelog_cnt *
+				     mdd->mdd_tod[op].mod_credits);
         }
 }
 
@@ -290,6 +292,14 @@ int mdd_txn_init_credits(const struct lu_env *env, struct mdd_device *mdd)
                                      2 * dt[DTO_INDEX_DELETE] +
                                      dt[DTO_XATTR_SET];
                                 break;
+			case MDD_TXN_CHANGELOG_OP:
+				/* adding changelog record may require
+				 * object creation, rename and adding
+				 * 4 records 2 blocks max each */
+				*c = dt[DTO_OBJECT_CREATE] +
+					dt[DTO_OBJECT_RENAME] +
+					dt[DTO_WRITE_BLOCK] * 8;
+				break;
                         default:
                                 CERROR("Invalid op %d init its credit\n", op);
                                 LBUG();
