ac8a31b96510c4b64b9c880161f0f6deae2e9cc0
Vitaly Fertman <vitaly_fertman@xyratex.com>
2014-12-08 20:25:58 +0300

    Revert "MRP-2021 mdd: credits for changelog calculation"
    This reverts commit 058244057147a31b436b130839dedaf7ac8c34b6.

--- lustre/osd-ldiskfs/osd_handler.c
+++ lustre/osd-ldiskfs/osd_handler.c
@@ -1085,12 +1085,6 @@ static const int osd_dto_credits_noquota[DTO_NR] = {
          * 3(inode bits, groups, GDT)
          */
         [DTO_OBJECT_CREATE] = 25,
-        /*
-         * Rename an object. Same as rename in EXT3 filesystem.
-         * 2 * DATA_TRANS_BLOCKS(12) +
-         * INDEX_EXTRA_TRANS_BLOCKS(8) + 2
-         */
-        [DTO_OBJECT_RENAME] = 32,
         /**
          * Unused now
          */
@@ -1107,6 +1101,7 @@ static const int osd_dto_credits_noquota[DTO_NR] = {
          * are also counted in. Do not know why?
          */
         [DTO_XATTR_SET]     = 14,
+        [DTO_LOG_REC]       = 14,
         /**
          * creadits for inode change during write.
          */
@@ -1152,12 +1147,6 @@ static const int osd_dto_credits_quota[DTO_NR] = {
          */
         [DTO_OBJECT_CREATE] = 77,
         /*
-         * Rename an object. Same as rename in EXT3 filesystem.
-         * 2 * DATA_TRANS_BLOCKS(16) +
-         * INDEX_EXTRA_TRANS_BLOCKS(8) + 2
-         */
-        [DTO_OBJECT_RENAME] = 42,
-        /*
          * Unused now.
          * DATA_TRANS_BLOCKS(16) +
          * INDEX_EXTRA_BLOCKS(8) +
@@ -1177,6 +1166,7 @@ static const int osd_dto_credits_quota[DTO_NR] = {
          *           also counted in. Do not know why?
          */
         [DTO_XATTR_SET]     = 16,
+        [DTO_LOG_REC]       = 16,
         /**
          * creadits for inode change during write.
          */
