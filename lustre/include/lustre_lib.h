e763e0878675588d26f45633b70125f29e3173ed
Niu Yawei <yawei.niu@intel.com>
2015-01-11 06:01:31 -0500

    LU-5916 lod: inherit default pool setting properly
    When parent directory has no default striping but has only
    default pool setting, the pool setting should be inherited
    by subdir properly.
    
    This patch fixed the problem by changing the
    LOVEA_DELETE_VALUES() to check pool name as well, and it
    removed the unused function lod_store_def_striping().
    
    Lustre-commit: e8214d6bd4ccc3ff386c77904664c60f1848ef11
    Lustre-change: http://review.whamcloud.com/#/c/12746/
    
    Signed-off-by: Niu Yawei <yawei.niu@intel.com>
    Change-Id: I8ab9623772bb0d78965396daaab54353f07228f0
    Reviewed-on: http://review.whamcloud.com/13411
    Tested-by: Jenkins
    Reviewed-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/include/lustre_lib.h
+++ lustre/include/lustre_lib.h
@@ -110,8 +110,10 @@ void statfs_pack(struct obd_statfs *osfs, struct kstatfs *sfs);
 void statfs_unpack(struct kstatfs *sfs, struct obd_statfs *osfs);
 
 /* Special values for remove LOV EA from disk */
-#define LOVEA_DELETE_VALUES(size, count, offset) (size == 0 && count == 0 && \
-                                                 offset == (typeof(offset))(-1))
+#define LOVEA_DELETE_VALUES(size, count, offset, pool)			\
+	((size) == 0 && (count) == 0 &&					\
+	 (offset) == (typeof(offset))(-1) && (pool) == NULL)
+
 
 /* #define POISON_BULK 0 */
 
