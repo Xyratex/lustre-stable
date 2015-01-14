18e48896928c332c73b886e8826b648c9b9f634c
Andriy Skulysh <andriy.skulysh@seagate.com>
2015-01-14 11:46:40 +0200

    MRP-2352 ldlm: crash on umount in cleanup_resource
    cfs_hash_for_each_relax() assumes that cfs_hash_put_locked()
    doesn't release bd lock, but it isn't true for
    ldlm_res_hop_put_locked().
    
    Add recfcount on next hnode in cfs_hash_for_each_relax() and
    remove ldlm_res_hop_put_locked()
    
    Change-Id: I300a45ae1e5f9806dfcab9c780f84bb7f64146a3
    Signed-off-by: Andriy Skulysh <andriy.skulysh@seagate.com>
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/5022
    Tested-by: Jenkins
    Reviewed-by: Vitaly Fertman <vitaly.fertman@seagate.com>
    Reviewed-by: Alexander Boyko <alexander.boyko@seagate.com>
    Tested-by: Alexander Lezhoev <alexander.lezhoev@seagate.com>

--- lustre/ldlm/ldlm_internal.h
+++ lustre/ldlm/ldlm_internal.h
@@ -85,7 +85,6 @@ int ldlm_cancel_lru_local(struct ldlm_namespace *ns,
                           ldlm_cancel_flags_t cancel_flags, int flags);
 extern int ldlm_enqueue_min;
 /* ldlm_resource.c */
-int ldlm_resource_putref_locked(struct ldlm_resource *res);
 void ldlm_resource_insert_lock_after(struct ldlm_lock *original,
                                      struct ldlm_lock *new);
 /* ldlm_lock.c */
