f9f4324b1d36a231a8eee9f724f4cad50c0b5e10
wang di <di.wang@intel.com>
2015-03-30 19:43:23 -0700

    LU-6419 fld: fld client lookup should retry
    If FLD client lookup fails because of the remote target
    is shutdown (or deactive), it should retry another target,
    otherwise it will cause the application failure.
    
    And FLD client should stop retry if the import has
    been deactive.
    
    Signed-off-by: wang di <di.wang@intel.com>
    Change-Id: Ic5934054bf0aff3158b6fe040fcb26699336f038
    Reviewed-on: http://review.whamcloud.com/14313
    Tested-by: Jenkins
    Reviewed-by: Lai Siyao <lai.siyao@intel.com>
    Reviewed-by: Fan Yong <fan.yong@intel.com>
    Reviewed-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/fld/fld_request.c
+++ lustre/fld/fld_request.c
@@ -465,7 +465,7 @@ again:
         if (fld_op != FLD_LOOKUP)
                 mdc_put_rpc_lock(exp->exp_obd->u.cli.cl_rpc_lock, NULL);
 	if (rc != 0) {
-		if (imp->imp_state != LUSTRE_IMP_CLOSED) {
+		if (imp->imp_state != LUSTRE_IMP_CLOSED && !imp->imp_deactive) {
 			/* Since LWP is not replayable, so it will keep
 			 * trying unless umount happens, otherwise it would
 			 * cause unecessary failure of the application. */
@@ -491,6 +491,7 @@ int fld_client_lookup(struct lu_client_fld *fld, u64 seq, u32 *mds,
 {
 	struct lu_seq_range res = { 0 };
 	struct lu_fld_target *target;
+	struct lu_fld_target *origin;
 	int rc;
 	ENTRY;
 
@@ -505,7 +506,8 @@ int fld_client_lookup(struct lu_client_fld *fld, u64 seq, u32 *mds,
         /* Can not find it in the cache */
         target = fld_client_get_target(fld, seq);
         LASSERT(target != NULL);
-
+	origin = target;
+again:
         CDEBUG(D_INFO, "%s: Lookup fld entry (seq: "LPX64") on "
                "target %s (idx "LPU64")\n", fld->lcf_name, seq,
                fld_target_name(target), target->ft_idx);
@@ -523,6 +525,22 @@ int fld_client_lookup(struct lu_client_fld *fld, u64 seq, u32 *mds,
 		rc = fld_client_rpc(target->ft_exp, &res, FLD_LOOKUP);
 	}
 
+	if (rc == -ESHUTDOWN) {
+		/* If fld lookup failed because the target has been shutdown,
+		 * then try next target in the list, until trying all targets
+		 * or fld lookup succeeds */
+		spin_lock(&fld->lcf_lock);
+		if (target->ft_chain.next == fld->lcf_targets.prev)
+			target = list_entry(fld->lcf_targets.next,
+					    struct lu_fld_target, ft_chain);
+		else
+			target = list_entry(target->ft_chain.next,
+						 struct lu_fld_target,
+						 ft_chain);
+		spin_unlock(&fld->lcf_lock);
+		if (target != origin)
+			goto again;
+	}
 	if (rc == 0) {
 		*mds = res.lsr_index;
 		fld_cache_insert(fld->lcf_cache, &res);
