5bff85f51d7a2c7c037e855a2415574fe3121d40
Hongchao Zhang <hongchao.zhang@intel.com>
2015-04-19 04:56:35 +0800

    LU-5407 test: wait MGC import to finish recovery
    in test_58c of replay_single.sh, the MGS is failed over with MDS
    by the above subtest "test_58b", and the import of MGC could not
    detect it yet, then causes the second Lustre mount to be failed.
    
    this patch also remove test 58c from ALWAYS_EXCEPT list of ZFS
    
    Change-Id: I7311e122964d6d8e14fa4327ed3ad936ac4ef59b
    Signed-off-by: Hongchao Zhang <hongchao.zhang@intel.com>
    Reviewed-on: http://review.whamcloud.com/14792
    Tested-by: Jenkins
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Nathaniel Clark <nathaniel.l.clark@intel.com>

--- lustre/tests/replay-single.sh
+++ lustre/tests/replay-single.sh
@@ -1447,24 +1447,28 @@ test_58b() {
     rm -f $DIR/$tdir/$tfile
     rmdir $DIR/$tdir
     cleanup_58
+    wait_clients_import_state ${CLIENTS:-$HOSTNAME} "mgs" FULL
 }
 run_test 58b "test replay of setxattr op"
 
 test_58c() { # bug 16570
-    local orig
-    local orig1
-    local new
+	local orig
+	local orig1
+	local new
 
-    trap cleanup_58 EXIT
+	trap cleanup_58 EXIT
 
-    if large_xattr_enabled; then
-        local xattr_size=$(max_xattr_size)
-        orig="$(generate_string $((xattr_size / 2)))"
-        orig1="$(generate_string $xattr_size)"
-    else
-        orig="bar"
-        orig1="bar1"
-    fi
+	if large_xattr_enabled; then
+		local xattr_size=$(max_xattr_size)
+		orig="$(generate_string $((xattr_size / 2)))"
+		orig1="$(generate_string $xattr_size)"
+	else
+		orig="bar"
+		orig1="bar1"
+	fi
+
+	# PING_INTERVAL max(obd_timeout / 4, 1U)
+	sleep $((TIMEOUT / 4))
 
     mount_client $MOUNT2
     mkdir -p $DIR/$tdir
