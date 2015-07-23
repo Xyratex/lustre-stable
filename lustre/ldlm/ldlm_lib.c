fcbb5952562d61d6434bca0b541fe3602d7f3718
Niu Yawei <yawei.niu@intel.com>
2015-07-23 04:20:03 -0400

    LU-6629 recovery: handle LWP client reconnect properly
    In target_handle_connect(), it checks LWP client only for the
    inital connect, which makes the MSG_CONNECT_RECONNECT being
    returned to client on reconnect, and this will break LWP users
    such as quota:
    
    Quota needs to invalidate global quota locks on every reconnect
    (the import state should always be set to LUSTRE_IMP_EVICTED for
    each connect/reconnect), so that to ensure the quota index files
    are synced between master and slave after reconnection, setting
    MSG_CONNECT_RECONNECT flag mistakenly will make the LWP client
    import enter a wrong state rather than LUSTRE_IMP_EVICTED.
    
    Another problem is that it could lead to LWP client can't connect
    during recovery in certain situation.
    
    Test-Parameters: alwaysuploadlogs \
    envdefinitions=ONLY=5 \
    mdtfilesystemtype=ldiskfs mdsfilesystemtype=ldiskfs ostfilesystemtype=ldiskfs \
    clientdistro=el7 ossdistro=el7 mdsdistro=el7 \
    mdscount=2 mdtcount=4 testlist=sanity-scrub
    
    Signed-off-by: Niu Yawei <yawei.niu@intel.com>
    Change-Id: I2292ab16e60043e6ddcce0938a8db8d139344074
    Reviewed-on: http://review.whamcloud.com/15695
    Reviewed-by: Bobi Jam <bobijam@hotmail.com>
    Reviewed-by: wangdi <di.wang@intel.com>
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/ldlm/ldlm_lib.c
+++ lustre/ldlm/ldlm_lib.c
@@ -980,10 +980,10 @@ int target_handle_connect(struct ptlrpc_request *req)
 		}
 	}
 
+	lw_client = (data->ocd_connect_flags & OBD_CONNECT_LIGHTWEIGHT) != 0;
+
 	if (lustre_msg_get_op_flags(req->rq_reqmsg) & MSG_CONNECT_INITIAL) {
 		mds_conn = (data->ocd_connect_flags & OBD_CONNECT_MDS) != 0;
-		lw_client = (data->ocd_connect_flags &
-			     OBD_CONNECT_LIGHTWEIGHT) != 0;
 	}
 
         /* lctl gets a backstage, all-access pass. */
