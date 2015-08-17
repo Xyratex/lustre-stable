9281baef6fb69e3618360e3a5cc7f471a09d6dce
Elena Gryaznova <elena.gryaznova@seagate.com>
2015-08-17 14:13:38 +0300

    MRP-2828 test: skip server setup for NOSETUP mode
    t-f does not have a possibility to skip the server setup and
    mount clients only, this possibility exists only on CLIENTONLY
    mode which is not widely used.
    
    Signed-off-by: Elena Gryaznova <elena.gryaznova@seagate.com>
    Change-Id: Ib2a7261e9677d7e6cc6bc70afac5128f184c02d7
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/7982
    Tested-by: Jenkins
    Tested-by: Alexander Lezhoev <alexander.lezhoev@seagate.com>
    Reviewed-by: Alexander Lezhoev <alexander.lezhoev@seagate.com>
    Reviewed-by: Vitaly Fertman <vitaly.fertman@seagate.com>

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -4622,7 +4622,7 @@ setupall() {
 
 	init_gss
 
-	if [ -z "$CLIENTONLY" ]; then
+	if [[ -z "$CLIENTONLY" && -z "$NOSETUP" ]]; then
 		echo Setup mgs, mdt, osts
 		echo $WRITECONF | grep -q "writeconf" && writeconf_all
 
