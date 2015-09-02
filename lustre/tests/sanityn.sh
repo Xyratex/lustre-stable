63e29fffe66b38c3c675719147b7302339eb1301
Henri Doreau <henri.doreau@cea.fr>
2015-09-02 10:55:40 +0200

    LU-7072 tests: Fix and re-enable test 78 in sanityn
    Early NRS policy tunings can fail for the following reasons:
      - Policy not started (ENODEV)
      - Policy not fully initialized (EAGAIN)
    
    Make test_78 of sanityn handle both properly and re-enable it.
    
    Signed-off-by: Henri Doreau <henri.doreau@cea.fr>
    Change-Id: I1d68ad709d0ac1c8ee9dabebf565c786d7c904da
    Reviewed-on: http://review.whamcloud.com/16178
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Tested-by: Jenkins
    Tested-by: Andreas Dilger <andreas.dilger@intel.com>

--- lustre/tests/sanityn.sh
+++ lustre/tests/sanityn.sh
@@ -3116,8 +3116,12 @@ test_78() { #LU-6673
 		do_facet ost"$i" lctl set_param \
 			ost.OSS.*.nrs_orr_quantum=1
 		rc=$?
-		[ $rc -eq 0 -o $rc -eq 11 ] ||
-			error "Expected set_param to return 0 or EAGAIN"
+		# Valid return codes are:
+		# 0: Tuning succeeded
+		# ENODEV: Policy is still stopped
+		# EAGAIN: Policy is being initialized
+		[ $rc -eq 0 -o $rc -eq 19 -o $rc -eq 11 ] ||
+			error "Expected set_param to return 0|ENODEV|EAGAIN"
 	done
 
 	# Cleanup the ORR policy
