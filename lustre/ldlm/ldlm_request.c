041cfffd752b856afb4b84b2400f4a58d59d5671
Vitaly Fertman <vitaly.fertman@seagate.com>
2015-11-26 22:00:24 +0300

    MRP-3169 ldlm: add debug to ldlm_completion_ast
    to see what we are blocked by in the resource
    
    Signed-off-by: Vitaly Fertman <vitaly.fertman@seagate.com>
    Change-Id: I86944e684af246d20cd3b15ec6fc53f9c6f102ab
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/9151
    Tested-by: Jenkins
    Reviewed-by: Andriy Skulysh <andriy.skulysh@seagate.com>
    Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@seagate.com>
    Tested-by: Elena V. Gryaznova <elena.gryaznova@seagate.com>

--- lustre/ldlm/ldlm_request.c
+++ lustre/ldlm/ldlm_request.c
@@ -93,10 +93,12 @@ int ldlm_expired_completion_wait(void *data)
 			   cfs_time_sub(cfs_time_current_sec(),
 					lock->l_last_activity));
                 if (cfs_time_after(cfs_time_current(), next_dump)) {
-                        last_dump = next_dump;
-                        next_dump = cfs_time_shift(300);
-                        ldlm_namespace_dump(D_DLMTRACE,
-                                            ldlm_lock_to_ns(lock));
+			last_dump = next_dump;
+			next_dump = cfs_time_shift(30);
+
+			lock_res(lock->l_resource);
+			ldlm_resource_dump(D_DLMTRACE, lock->l_resource);
+			unlock_res(lock->l_resource);
                         if (last_dump == 0)
                                 libcfs_debug_dumplog();
                 }
