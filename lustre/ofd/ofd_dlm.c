681eb90159f92c1cbac7926342beae8c4650fb5f
Vitaly Fertman <vitaly.fertman@seagate.com>
2016-02-29 19:26:03 +0300

    MRP-2960 ldlm: Implement lock ahead
    This is the server side of the lock ahead feature.
    The client side of this feature is not compatible with
    Lustre 2.5.
    
    The server side patch has the following components:
    New LDLM flag, LDLM_FL_NO_EXPANSION, telling the server not
    to expand extent locks requests with this flag set
    Changes to lock matching code to allow non-blocking lock
    requests for all lock types (not just group locks)
    Changes to the ofd intent code to get correct file sizes
    with locks in advance of IO
    
    The description of the full feature (including client code)
    is here:
    http://review.whamcloud.com/#/c/13564/
    
    Signed-off-by: Patrick Farrell <paf@cray.com>
    Signed-off-by: Vitaly Fertman <vitaly.fertman@seagate.com>
    Change-Id: Iba6fd5fc3190bab75e5d9a6b7bc48e267a32341b
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/9909
    Tested-by: Jenkins
    Tested-by: Elena V. Gryaznova <elena.gryaznova@seagate.com>

--- lustre/ofd/ofd_dlm.c
+++ lustre/ofd/ofd_dlm.c
@@ -45,10 +45,30 @@
 #include "ofd_internal.h"
 
 struct ofd_intent_args {
-	struct ldlm_lock	**victim;
+	struct list_head	gl_list;
 	__u64			 size;
-	int			*liblustre;
+	int			liblustre;
+	int			no_glimpse_ast;
 };
+
+int ofd_dlm_init(void)
+{
+	ldlm_glimpse_work_kmem = kmem_cache_create("ldlm_glimpse_work_kmem",
+					  sizeof(struct ldlm_glimpse_work),
+					  0, 0, NULL);
+	if (ldlm_glimpse_work_kmem == NULL)
+		return -ENOMEM;
+	else
+		return 0;
+}
+
+void ofd_dlm_exit(void)
+{
+	if (ldlm_glimpse_work_kmem) {
+		kmem_cache_destroy(ldlm_glimpse_work_kmem);
+		ldlm_glimpse_work_kmem = NULL;
+	}
+}
 
 /**
  * OFD interval callback.
@@ -58,8 +78,37 @@ struct ofd_intent_args {
  * covering extents beyond the given args->size. This is used to decide if LVB
  * data is outdated.
  *
+ * It finds the highest lock (by starting point) in this interval, and adds it
+ * to the list of locks to glimpse.  We must glimpse a list of locks - rather
+ * than only the highest lock on the file - because lock ahead creates extent
+ * locks in advance of IO, and so breaks the assumption that the holder of the
+ * highest lock knows the current file size.
+ *
+ * This assumption is normally true because locks which are created as part of
+ * IO - rather than in advance of it - are guaranteed to be 'active', IE,
+ * involved in IO, and the highest 'active' lock always knows the current file
+ * size, because it is either not changing or the holder of that lock is
+ * responsible for updating it.
+ *
+ * So we need only glimpse until we find the first 'active' lock.
+ * Unfortunately, there is no way to know if a lock ahead/speculative lock is
+ * 'active' from the server side.  So we must glimpse all speculative locks we
+ * find and merge their LVBs.
+ *
+ * However, *all* non-speculative locks are active.  So we can stop glimpsing
+ * as soon as we find a non-speculative lock.  Currently, all speculative locks
+ * have LDLM_FL_NO_EXPANSION set, and we use this to identify them.  This is
+ * enforced by an assertion in osc_lock_init, which references this comment.
+ *
+ * If that ever changes, we will either need to find a new way to identify
+ * active locks or we will need to glimpse all PW locks.
+ *
+ * Note that it is safe to glimpse only the 'top' lock from each interval
+ * because ofd_intent_cb is only called for PW extent locks, and for PW locks,
+ * there is only one lock per interval.
+ *
  * \param[in] n		interval node
- * \param[in] args	intent arguments
+ * \param[in,out] args	intent arguments, gl work list for identified locks
  *
  * \retval		INTERVAL_ITER_STOP if the interval is lower than
  *			file size, caller stops execution
@@ -71,13 +120,15 @@ static enum interval_iter ofd_intent_cb(struct interval_node *n, void *args)
 	struct ldlm_interval	 *node = (struct ldlm_interval *)n;
 	struct ofd_intent_args	 *arg = args;
 	__u64			  size = arg->size;
-	struct ldlm_lock	**v = arg->victim;
+	struct ldlm_lock	*v = NULL;
 	struct ldlm_lock	 *lck;
+	struct ldlm_glimpse_work *gl_work;
 
 	/* If the interval is lower than the current file size, just break. */
 	if (interval_high(n) <= size)
 		return INTERVAL_ITER_STOP;
 
+	/* Find the 'victim' lock from this interval */
 	list_for_each_entry(lck, &node->li_group, l_sl_policy) {
 		/* Don't send glimpse ASTs to liblustre clients.
 		 * They aren't listening for them, and they do
@@ -85,23 +136,48 @@ static enum interval_iter ofd_intent_cb(struct interval_node *n, void *args)
 		if (lck->l_export == NULL || lck->l_export->exp_libclient)
 			continue;
 
-		if (*arg->liblustre)
-			*arg->liblustre = 0;
+		if (arg->liblustre)
+			arg->liblustre = 0;
 
-		if (*v == NULL) {
-			*v = LDLM_LOCK_GET(lck);
-		} else if ((*v)->l_policy_data.l_extent.start <
-			   lck->l_policy_data.l_extent.start) {
-			LDLM_LOCK_RELEASE(*v);
-			*v = LDLM_LOCK_GET(lck);
-		}
+		v = LDLM_LOCK_GET(lck);
 
 		/* the same policy group - every lock has the
 		 * same extent, so needn't do it any more */
 		break;
 	}
 
-	return INTERVAL_ITER_CONT;
+	/*
+	 * This check is for lock taken in ofd_destroy_by_fid() that does
+	 * not have l_glimpse_ast set. So the logic is: if there is a lock
+	 * with no l_glimpse_ast set, this object is being destroyed already.
+	 * Hence, if you are grabbing DLM locks on the server, always set
+	 * non-NULL glimpse_ast (e.g., ldlm_request.c::ldlm_glimpse_ast()).
+	 */
+	if (v->l_glimpse_ast == NULL) {
+		LDLM_DEBUG(v, "no l_glimpse_ast");
+		arg->no_glimpse_ast = 1;
+		LDLM_LOCK_RELEASE(v);
+		return INTERVAL_ITER_STOP;
+	}
+
+	OBD_SLAB_ALLOC_PTR_GFP(gl_work, ldlm_glimpse_work_kmem, GFP_ATOMIC);
+
+	/* Populate the gl_work structure. */
+	gl_work->gl_lock = v;
+	list_add_tail(&gl_work->gl_list, &arg->gl_list);
+	/* There is actually no need for a glimpse descriptor when glimpsing
+	 * extent locks */
+	gl_work->gl_desc = NULL;
+	/* This tells ldlm_work_gl_ast_lock this was allocated from a slab and
+	 * must be freed in a slab-aware manner. */
+	gl_work->gl_flags = LDLM_GL_WORK_SLAB_ALLOCATED;
+
+	/* If NO_EXPANSION is not set, this is an active lock, and we don't need
+	 * to glimpse any further.  See comment above this function. */
+	if (!(v->l_flags & LDLM_FL_NO_EXPANSION))
+		return INTERVAL_ITER_STOP;
+	else
+		return INTERVAL_ITER_CONT;
 }
 
 /**
@@ -131,13 +207,13 @@ int ofd_intent_policy(struct ldlm_namespace *ns, struct ldlm_lock **lockp,
 		      void *data)
 {
 	struct ptlrpc_request		*req = req_cookie;
-	struct ldlm_lock		*lock = *lockp, *l = NULL;
+	struct ldlm_lock		*lock = *lockp;
 	struct ldlm_resource		*res = lock->l_resource;
 	ldlm_processing_policy		 policy;
 	struct ost_lvb			*res_lvb, *reply_lvb;
 	struct ldlm_reply		*rep;
 	ldlm_error_t			 err;
-	int				 idx, rc, only_liblustre = 1;
+	int				 idx, rc;
 	struct ldlm_interval_tree	*tree;
 	struct ofd_intent_args		 arg;
 	__u32				 repsize[3] = {
@@ -145,11 +221,12 @@ int ofd_intent_policy(struct ldlm_namespace *ns, struct ldlm_lock **lockp,
 		[DLM_LOCKREPLY_OFF]   = sizeof(*rep),
 		[DLM_REPLY_REC_OFF]   = sizeof(*reply_lvb)
 	};
-	struct ldlm_glimpse_work	 gl_work;
-	struct list_head		 gl_list;
+	struct ldlm_glimpse_work	 *pos, *tmp;
 	ENTRY;
 
-	INIT_LIST_HEAD(&gl_list);
+	INIT_LIST_HEAD(&arg.gl_list);
+	arg.no_glimpse_ast = 0;
+	arg.liblustre = 1;
 	lock->l_lvb_type = LVB_T_OST;
 	policy = ldlm_get_processing_policy(res);
 	LASSERT(policy != NULL);
@@ -233,9 +310,9 @@ int ofd_intent_policy(struct ldlm_namespace *ns, struct ldlm_lock **lockp,
 	 *  res->lr_lvb_sem.
 	 */
 	arg.size = reply_lvb->lvb_size;
-	arg.victim = &l;
-	arg.liblustre = &only_liblustre;
 
+	/* Check for PW locks beyond the size in the LVB, build the list
+	 * of locks to glimpse (arg.gl_list) */
 	for (idx = 0; idx < LCK_MODE_NUM; idx++) {
 		tree = &res->lr_itree[idx];
 		if (tree->lit_mode == LCK_PR)
@@ -246,8 +323,8 @@ int ofd_intent_policy(struct ldlm_namespace *ns, struct ldlm_lock **lockp,
 	unlock_res(res);
 
 	/* There were no PW locks beyond the size in the LVB; finished. */
-	if (l == NULL) {
-		if (only_liblustre) {
+	if (list_empty(&arg.gl_list)) {
+		if (arg.liblustre) {
 			/* If we discovered a liblustre client with a PW lock,
 			 * however, the LVB may be out of date!  The LVB is
 			 * updated only on glimpse (which we don't do for
@@ -263,43 +340,27 @@ int ofd_intent_policy(struct ldlm_namespace *ns, struct ldlm_lock **lockp,
 		RETURN(ELDLM_LOCK_ABORTED);
 	}
 
-	/*
-	 * This check is for lock taken in ofd_destroy_by_fid() that does
-	 * not have l_glimpse_ast set. So the logic is: if there is a lock
-	 * with no l_glimpse_ast set, this object is being destroyed already.
-	 * Hence, if you are grabbing DLM locks on the server, always set
-	 * non-NULL glimpse_ast (e.g., ldlm_request.c::ldlm_glimpse_ast()).
-	 */
-	if (l->l_glimpse_ast == NULL) {
+	if (arg.no_glimpse_ast) {
 		/* We are racing with unlink(); just return -ENOENT */
 		rep->lock_policy_res1 = ptlrpc_status_hton(-ENOENT);
-		goto out;
+		GOTO(out, ELDLM_LOCK_ABORTED);
 	}
 
-	/* Populate the gl_work structure.
-	 * Grab additional reference on the lock which will be released in
-	 * ldlm_work_gl_ast_lock() */
-	gl_work.gl_lock = LDLM_LOCK_GET(l);
-	/* The glimpse callback is sent to one single extent lock. As a result,
-	 * the gl_work list is just composed of one element */
-	list_add_tail(&gl_work.gl_list, &gl_list);
-	/* There is actually no need for a glimpse descriptor when glimpsing
-	 * extent locks */
-	gl_work.gl_desc = NULL;
-	/* the ldlm_glimpse_work structure is allocated on the stack */
-	gl_work.gl_flags = LDLM_GL_WORK_NOFREE;
-
-	rc = ldlm_glimpse_locks(res, &gl_list); /* this will update the LVB */
-
-	if (!list_empty(&gl_list))
-		LDLM_LOCK_RELEASE(l);
+	/* this will update the LVB */
+	rc = ldlm_glimpse_locks(res, &arg.gl_list);
 
 	lock_res(res);
 	*reply_lvb = *res_lvb;
 	unlock_res(res);
 
 out:
-	LDLM_LOCK_RELEASE(l);
+	/* If the list is not empty, we failed to glimpse some locks and
+	 * must clean up.  Usually due to a race with unlink.*/
+	list_for_each_entry_safe(pos, tmp, &arg.gl_list, gl_list) {
+		list_del(&pos->gl_list);
+		LDLM_LOCK_RELEASE(pos->gl_lock);
+		OBD_SLAB_FREE_PTR(pos, ldlm_glimpse_work_kmem);
+	}
 
 	RETURN(ELDLM_LOCK_ABORTED);
 }
