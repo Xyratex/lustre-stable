f94951c79410ea9b6680ba824a4189e187a2a962
Vladimir Saveliev <vladimir.saveliev@seagate.com>
2016-03-18 17:45:30 +0300

    MRP-2930 mgc: take comma as separator of mgsnode's list
    mgsnode's list of nids is allowed to use comma as separator, however
    lustre_start_mgc() assumes that only colon can be used. Due to that
    nids separated by comma do not get into list of import's connections.
    
    Signed-off-by: Vladimir Saveliev <vladimir.saveliev@seagate.com>
    Change-Id: I3c10250f67ad06719f417ba756c98396c6e7cf53
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/10171
    Tested-by: Jenkins
    Tested-by: Elena V. Gryaznova <elena.gryaznova@seagate.com>
    Reviewed-by: Sergey Cheremencev <sergey.cheremencev@seagate.com>
    Reviewed-by: Alexey Leonidovich Lyashkov <alexey.lyashkov@seagate.com>

--- lustre/obdclass/obd_mount.c
+++ lustre/obdclass/obd_mount.c
@@ -387,7 +387,7 @@ int lustre_start_mgc(struct super_block *sb)
 			if (rc == 0)
 				++i;
                         /* Stop at the first failover nid */
-                        if (*ptr == ':')
+			if (*ptr == ':' || *ptr == ',')
                                 break;
                 }
         }
@@ -411,7 +411,7 @@ int lustre_start_mgc(struct super_block *sb)
 
         /* Add any failover MGS nids */
         i = 1;
-	while (ptr && ((*ptr == ':' ||
+	while (ptr && ((*ptr == ':' || *ptr == ',' ||
 	       class_find_param(ptr, PARAM_MGSNODE, &ptr) == 0))) {
 		/* New failover node */
 		sprintf(niduuid, "%s_%x", mgcname, i);
@@ -421,7 +421,7 @@ int lustre_start_mgc(struct super_block *sb)
 				     niduuid, 0, 0, 0);
 			if (rc == 0)
 				++j;
-			if (*ptr == ':')
+			if (*ptr == ':' || *ptr == ',')
 				break;
 		}
 		if (j > 0) {
