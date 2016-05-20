c69af437c90fd902ff37aae0990bf382e2842ef9
Parinay Kondekar <parinay.kondekar@seagate.com>
2016-05-20 22:55:13 +0530

    MRP-3501 o2iblnd: handle new struct ib_cq_init_attr
    For linux kernel version 4.2 the infiniband layer
    created a new structure ib_cq_init_attr to pass to
    ib_create_cq(). This patch handles this new case.
    See linux commit
     commit 8e37210b38fb7d6a ("IB/core: Change
    	ib_create_cq to use struct ib_cq_init_attr")
    
    Change-Id: I537eed49a96caa0473b818236c9ce8674937d3af
    Intel-bug-id: LU-6215
    Signed-off-by: James Simmons <uja.ornl@yahoo.com>
    Signed-off-by: Parinay Kondekar <parinay.kondekar@seagate.com>
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/10904
    Reviewed-by: Olena Romanenko <alyona.romanenko@seagate.com>
    Reviewed-by: Rahul Deshmukh <rahul.deshmukh@seagate.com>
    Tested-by: Jenkins

--- lnet/klnds/o2iblnd/o2iblnd.c
+++ lnet/klnds/o2iblnd/o2iblnd.c
@@ -708,6 +708,9 @@ kiblnd_create_conn(kib_peer_t *peer, struct rdma_cm_id *cmid,
 	kib_dev_t              *dev;
 	struct ib_qp_init_attr *init_qp_attr;
 	struct kib_sched_info	*sched;
+#ifdef HAVE_IB_CQ_INIT_ATTR
+	struct ib_cq_init_attr  cq_attr = {};
+#endif
 	kib_conn_t		*conn;
 	struct ib_cq		*cq;
 	unsigned long		flags;
@@ -804,12 +807,18 @@ kiblnd_create_conn(kib_peer_t *peer, struct rdma_cm_id *cmid,
 		goto failed_2;
 
 	kiblnd_map_rx_descs(conn);
-
+#ifdef HAVE_IB_CQ_INIT_ATTR
+	cq_attr.cqe = IBLND_CQ_ENTRIES(version);
+	cq_attr.comp_vector = kiblnd_get_completion_vector(conn, cpt);
+	cq = ib_create_cq(cmid->device,
+			  kiblnd_cq_completion, kiblnd_cq_event, conn,
+			  &cq_attr);
+#else
 	cq = ib_create_cq(cmid->device,
 			  kiblnd_cq_completion, kiblnd_cq_event, conn,
 			  IBLND_CQ_ENTRIES(conn),
 			  kiblnd_get_completion_vector(conn, cpt));
-
+#endif
 	if (IS_ERR(cq)) {
 		CERROR("Failed to create CQ with %d CQEs: %ld\n",
 			IBLND_CQ_ENTRIES(conn), PTR_ERR(cq));
