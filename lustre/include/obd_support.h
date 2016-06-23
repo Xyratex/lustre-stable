d10f51e3cc26ee44af40b2d69a00e886f159d26e
Rahul Deshmkuh <rahul.deshmukh@seagate.com>
2016-06-23 10:22:40 +0530

    MRP-3480 test: RR policy should not allocate on same ost
    This is test case trying to create a situation where
    Round Robin (RR) policy will create objects on same ost
    
    Signed-off-by: Rahul Deshmukh <rahul.deshmukh@seagate.com>
    Change-Id: Idc3255c8251354adcfdd5bdf1095cf1ed8956e03
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/11355
    Tested-by: Jenkins
    Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@seagate.com>
    Reviewed-by: Andrew Perepechko <andrew.perepechko@seagate.com>
    Tested-by: Alexander Lezhoev <alexander.lezhoev@seagate.com>

--- lustre/include/obd_support.h
+++ lustre/include/obd_support.h
@@ -243,6 +243,7 @@ int obd_alloc_fail(const void *ptr, const char *name, const char *type,
 #define OBD_FAIL_MDS_INTENT_DELAY		0x160
 #define OBD_FAIL_MDS_XATTR_REP			0x161
 #define OBD_FAIL_MDS_TRACK_OVERFLOW	 0x162
+#define OBD_FAIL_MDS_LOV_CREATE_RACE	 0x163
 
 /* layout lock */
 #define OBD_FAIL_MDS_NO_LL_GETATTR	 0x170
