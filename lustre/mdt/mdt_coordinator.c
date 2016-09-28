f429a9a0cdfe2adb0eacedafcb41fa55224ac69b
Frank Zago <fzago@cray.com>
2016-09-28 11:08:07 -0400

    LU-7988 hsm: don't use a ptlrpc_thread struct for the cdt
    Since only the waitq and the event flag from the ptlrpc_thread
    structure are used, declare these directly and do not use the
    ptlrpc_thread structure at all.
    
    Test-Parameters: trivial testlist=sanity-hsm
    Signed-off-by: frank zago <fzago@cray.com>
    Change-Id: Iba63ac70ad381f7e40976ac423594eae46c5249b
    Reviewed-on: https://review.whamcloud.com/22761
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Henri Doreau <henri.doreau@cea.fr>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/mdt/mdt_coordinator.c
+++ lustre/mdt/mdt_coordinator.c
@@ -430,8 +430,8 @@ static int mdt_coordinator(void *data)
 	int			 request_sz;
 	ENTRY;
 
-	cdt->cdt_thread.t_flags = SVC_RUNNING;
-	wake_up(&cdt->cdt_thread.t_ctl_waitq);
+	cdt->cdt_flags = SVC_RUNNING;
+	wake_up(&cdt->cdt_waitq);
 
 	CDEBUG(D_HSM, "%s: coordinator thread starting, pid=%d\n",
 	       mdt_obd_name(mdt), current_pid());
@@ -455,23 +455,22 @@ static int mdt_coordinator(void *data)
 
 		lwi = LWI_TIMEOUT(cfs_time_seconds(cdt->cdt_loop_period),
 				  NULL, NULL);
-		l_wait_event(cdt->cdt_thread.t_ctl_waitq,
-			     (cdt->cdt_thread.t_flags &
-			      (SVC_STOPPING|SVC_EVENT)),
+		l_wait_event(cdt->cdt_waitq,
+			     cdt->cdt_flags & (SVC_STOPPING|SVC_EVENT),
 			     &lwi);
 
 		CDEBUG(D_HSM, "coordinator resumes\n");
 
-		if (cdt->cdt_thread.t_flags & SVC_STOPPING ||
+		if (cdt->cdt_flags & SVC_STOPPING ||
 		    cdt->cdt_state == CDT_STOPPING) {
-			cdt->cdt_thread.t_flags &= ~SVC_STOPPING;
+			cdt->cdt_flags &= ~SVC_STOPPING;
 			rc = 0;
 			break;
 		}
 
 		/* wake up before timeout, new work arrives */
-		if (cdt->cdt_thread.t_flags & SVC_EVENT)
-			cdt->cdt_thread.t_flags &= ~SVC_EVENT;
+		if (cdt->cdt_flags & SVC_EVENT)
+			cdt->cdt_flags &= ~SVC_EVENT;
 
 		/* if coordinator is suspended continue to wait */
 		if (cdt->cdt_state == CDT_DISABLE) {
@@ -584,8 +583,8 @@ out:
 		 * by mdt_stop_coordinator(), we have to ack
 		 * and cdt cleaning will be done by event sender
 		 */
-		cdt->cdt_thread.t_flags = SVC_STOPPED;
-		wake_up(&cdt->cdt_thread.t_ctl_waitq);
+		cdt->cdt_flags = SVC_STOPPED;
+		wake_up(&cdt->cdt_waitq);
 	}
 
 	if (rc != 0)
@@ -767,8 +766,8 @@ int mdt_hsm_cdt_wakeup(struct mdt_device *mdt)
 		RETURN(-ESRCH);
 
 	/* wake up coordinator */
-	cdt->cdt_thread.t_flags = SVC_EVENT;
-	wake_up(&cdt->cdt_thread.t_ctl_waitq);
+	cdt->cdt_flags = SVC_EVENT;
+	wake_up(&cdt->cdt_waitq);
 
 	RETURN(0);
 }
@@ -788,7 +787,7 @@ int mdt_hsm_cdt_init(struct mdt_device *mdt)
 
 	cdt->cdt_state = CDT_STOPPED;
 
-	init_waitqueue_head(&cdt->cdt_thread.t_ctl_waitq);
+	init_waitqueue_head(&cdt->cdt_waitq);
 	mutex_init(&cdt->cdt_llog_lock);
 	init_rwsem(&cdt->cdt_agent_lock);
 	init_rwsem(&cdt->cdt_request_lock);
@@ -913,8 +912,8 @@ static int mdt_hsm_cdt_start(struct mdt_device *mdt)
 		rc = 0;
 	}
 
-	wait_event(cdt->cdt_thread.t_ctl_waitq,
-		       (cdt->cdt_thread.t_flags & SVC_RUNNING));
+	wait_event(cdt->cdt_waitq,
+		       (cdt->cdt_flags & SVC_RUNNING));
 
 	cdt->cdt_state = CDT_RUNNING;
 	mdt->mdt_opts.mo_coordinator = 1;
@@ -942,10 +941,10 @@ int mdt_hsm_cdt_stop(struct mdt_device *mdt)
 
 	if (cdt->cdt_state != CDT_STOPPING) {
 		/* stop coordinator thread before cleaning */
-		cdt->cdt_thread.t_flags = SVC_STOPPING;
-		wake_up(&cdt->cdt_thread.t_ctl_waitq);
-		wait_event(cdt->cdt_thread.t_ctl_waitq,
-			   cdt->cdt_thread.t_flags & SVC_STOPPED);
+		cdt->cdt_flags = SVC_STOPPING;
+		wake_up(&cdt->cdt_waitq);
+		wait_event(cdt->cdt_waitq,
+			   cdt->cdt_flags & SVC_STOPPED);
 	}
 	cdt->cdt_state = CDT_STOPPED;
 
