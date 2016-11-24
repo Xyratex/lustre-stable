bfea23d8d7c4372a6dda08f5189c44cd61a695ba
Chennaiah Palla <chennaiah.palla@seagate.com>
2016-11-24 15:12:30 +0530

    MRP-3009 tests: racer test_1 to drop all error messages
    Filtered and dropped all Segmentation fault and Bus
    error messages. Used "export LANG=C" to display
    messages are in English instead of the local language.
    
    Signed-off-by: Chennaiah Palla <chennaiah.palla@seagate.com>
    Change-Id: I07a0e0358b4c72e2191076fe6a405a17d4ec4f80
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/13066
    Reviewed-by: Ashish Purkar <ashish.purkar@seagate.com>
    Reviewed-by: Elena V. Gryaznova <elena.gryaznova@seagate.com>
    Reviewed-by: Ujjwal Lanjewar <ujjwal.lanjewar@seagate.com>
    Tested-by: Elena V. Gryaznova <elena.gryaznova@seagate.com>

--- lustre/tests/racer/file_exec.sh
+++ lustre/tests/racer/file_exec.sh
@@ -2,14 +2,18 @@
 
 trap 'kill $(jobs -p)' EXIT
 
+org_LANG=$LANG
+export LANG=C
+
 DIR=$1
 MAX=$2
 PROG=/bin/sleep
 
 while /bin/true ; do
     file=$((RANDOM % MAX))
-    cp $PROG $DIR/$file > /dev/null 2>&1
+    cp -p $PROG $DIR/$file > /dev/null 2>&1
     $DIR/$file 0.$((RANDOM % 5 + 1)) 2> /dev/null
     sleep $((RANDOM % 3))
-done
+done 2>&1 | egrep -v "Segmentation fault|Bus error"
 
+export LANG=$org_LANG
