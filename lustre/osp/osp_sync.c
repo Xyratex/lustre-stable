lustre/lustre-release

LU-7001 osp: fix llog processing
The osp_sync_thread base on fact that llog_cat_process
will not end until umount. This is worng when processing reaches
bottom of catalog, or if catalog is wrapped.
The patch fixes this issue.

For wrapped catalog llog_process_thread could process old
record.
1 thread llog_process_thread read chunk and proccing first record
2 thread add rec to this catalog at this chunk and
  update bitmap
1 check bitmap for next idx and process old record

Test conf-sanity 106 was added.

Signed-off-by: Alexander Boyko <alexander.boyko@seagate.com>
Seagate-bug-id: MRP-4235
Change-Id: Ifc983018e3a325622ef3215bec4b69f5c9ac2ba2
Reviewed-on: https://review.whamcloud.com/26132
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Andriy Skulysh
Reviewed-by: Mike Pershin <mike.pershin@intel.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/osp/osp_sync.c
@@ -880,6 +880,8 @@ static void osp_sync_process_record(const struct lu_env *env,
 	cookie.lgc_subsys = LLOG_MDS_OST_ORIG_CTXT;
 	cookie.lgc_index = rec->lrh_index;
 
+	d->opd_sync_last_catalog_idx = llh->lgh_hdr->llh_cat_idx;
+
 	if (unlikely(rec->lrh_type == LLOG_GEN_REC)) {
 		struct llog_gen_rec *gen = (struct llog_gen_rec *)rec;
 
@@ -1167,6 +1169,7 @@ static int osp_sync_thread(void *_arg)
 	struct llog_handle	*llh;
 	struct lu_env		 env;
 	int			 rc, count;
+	bool			 wrapped;
 
 	ENTRY;
 
@@ -1201,7 +1204,35 @@ static int osp_sync_thread(void *_arg)
 		GOTO(out, rc = -EINVAL);
 	}
 
-	rc = llog_cat_process(&env, llh, osp_sync_process_queues, d, 0, 0);
+	/*
+	 * Catalog processing stops when it processed last catalog record
+	 * with index equal to the end of catalog bitmap. Or if it is wrapped,
+	 * processing stops with index equal to the lgh_last_idx. We need to
+	 * continue processing.
+	 */
+	d->opd_sync_last_catalog_idx = 0;
+	do {
+		int	size;
+
+		wrapped = (llh->lgh_hdr->llh_cat_idx >= llh->lgh_last_idx &&
+			   llh->lgh_hdr->llh_count > 1);
+
+		rc = llog_cat_process(&env, llh, osp_sync_process_queues, d,
+				      d->opd_sync_last_catalog_idx, 0);
+
+		size = OBD_FAIL_PRECHECK(OBD_FAIL_CAT_RECORDS) ?
+		       cfs_fail_val : (LLOG_HDR_BITMAP_SIZE(llh->lgh_hdr) - 1);
+		/* processing reaches catalog bottom */
+		if (d->opd_sync_last_catalog_idx == size)
+			d->opd_sync_last_catalog_idx = 0;
+		else if (wrapped)
+			/* If catalog is wrapped we can`t predict last index of
+			 * processing because lgh_last_idx could be changed.
+			 * Starting form the next one */
+			d->opd_sync_last_catalog_idx++;
+
+	} while (rc == 0 && (wrapped || d->opd_sync_last_catalog_idx == 0));
+
 	if (rc < 0) {
 		CERROR("%s: llog process with osp_sync_process_queues "
 		       "failed: %d\n", d->opd_obd->obd_name, rc);

lustre/tests/conf-sanity.sh
@@ -7462,6 +7462,32 @@ test_105() {
 }
 run_test 105 "check file creation for ro and rw bind mnt pt"
 
+test_106() {
+	local repeat=5
+
+	reformat
+	setupall
+	mkdir -p $DIR/$tdir || error "create $tdir failed"
+	lfs setstripe -c 1 -i 0 $DIR/$tdir
+#define OBD_FAIL_CAT_RECORDS                        0x1312
+	do_facet mds1 $LCTL set_param fail_loc=0x1312 fail_val=$repeat
+
+	for ((i = 1; i <= $repeat; i++)); do
+
+		#one full plain llog
+		createmany -o $DIR/$tdir/f- 64768
+
+		createmany -u $DIR/$tdir/f- 64768
+	done
+	wait_delete_completed $((TIMEOUT * 7))
+#ASSERTION osp_sync_thread() ( thread->t_flags != SVC_RUNNING ) failed
+#shows that osp code is buggy
+	do_facet mds1 $LCTL set_param fail_loc=0 fail_val=0
+
+	cleanupall
+}
+run_test 106 "check osp llog processing when catalog is wrapped"
+
 test_107() {
 	[[ $(lustre_version_code $SINGLEMDS) -ge $(version_code 2.10.50) ]] ||
 		{ skip "Need MDS version > 2.10.50"; return; }

Xyratex/lustre-stable

lustre/osp/osp_sync.c
@@ -801,6 +801,8 @@ static int osp_sync_process_record(const struct lu_env *env,
 	cookie.lgc_subsys = LLOG_MDS_OST_ORIG_CTXT;
 	cookie.lgc_index = rec->lrh_index;
 
+	d->opd_sync_last_catalog_idx = llh->lgh_hdr->llh_cat_idx;
+
 	if (unlikely(rec->lrh_type == LLOG_GEN_REC)) {
 		struct llog_gen_rec *gen = (struct llog_gen_rec *)rec;
 
@@ -1098,6 +1100,7 @@ static int osp_sync_thread(void *_arg)
 	struct llog_handle	*llh;
 	struct lu_env		 env;
 	int			 rc, count;
+	bool			 wrapped;
 
 	ENTRY;
 
@@ -1126,7 +1129,35 @@ static int osp_sync_thread(void *_arg)
 		GOTO(out, rc = -EINVAL);
 	}
 
-	rc = llog_cat_process(&env, llh, osp_sync_process_queues, d, 0, 0);
+	/*
+	 * Catalog processing stops when it processed last catalog record
+	 * with index equal to the end of catalog bitmap. Or if it is wrapped,
+	 * processing stops with index equal to the lgh_last_idx. We need to
+	 * continue processing.
+	 */
+	d->opd_sync_last_catalog_idx = 0;
+	do {
+		int	size;
+
+		wrapped = (llh->lgh_hdr->llh_cat_idx >= llh->lgh_last_idx &&
+			   llh->lgh_hdr->llh_count > 1);
+
+		rc = llog_cat_process(&env, llh, osp_sync_process_queues, d,
+				      d->opd_sync_last_catalog_idx, 0);
+
+		size = OBD_FAIL_PRECHECK(OBD_FAIL_CAT_RECORDS) ?
+		       cfs_fail_val : (LLOG_BITMAP_SIZE(llh->lgh_hdr) - 1);
+		/* processing reaches catalog bottom */
+		if (d->opd_sync_last_catalog_idx == size)
+			d->opd_sync_last_catalog_idx = 0;
+		else if (wrapped)
+			/* If catalog is wrapped we can`t predict last index of
+			 * processing because lgh_last_idx could be changed.
+			 * Starting form the next one */
+			d->opd_sync_last_catalog_idx++;
+
+	} while (rc == 0 && (wrapped || d->opd_sync_last_catalog_idx == 0));
+
 	LASSERTF(rc == 0 || rc == LLOG_PROC_BREAK,
 		 "%lu changes, %u in progress, %u in flight: %d\n",
 		 d->opd_syn_changes, d->opd_syn_rpc_in_progress,

lustre/tests/conf-sanity.sh
@@ -5750,6 +5750,32 @@ test_105()
 }
 run_test 105 "check failover after replace_nids"
 
+test_106() {
+	local repeat=5
+
+	reformat
+	setupall
+	mkdir -p $DIR/$tdir || error "create $tdir failed"
+	lfs setstripe -c 1 -i 0 $DIR/$tdir
+#define OBD_FAIL_CAT_RECORDS                        0x1313
+	do_facet mds1 $LCTL set_param fail_loc=0x1313 fail_val=$repeat
+
+	for ((i = 1; i <= $repeat; i++)); do
+
+		#one full plain llog
+		createmany -o $DIR/$tdir/f- 64768
+
+		createmany -u $DIR/$tdir/f- 64768
+	done
+	wait_delete_completed $((TIMEOUT * 7))
+#ASSERTION osp_sync_thread() ( thread->t_flags != SVC_RUNNING ) failed
+#shows that osp code is buggy
+	do_facet mds1 $LCTL set_param fail_loc=0 fail_val=0
+
+	cleanupall
+}
+run_test 106 "check osp llog processing when catalog is wrapped"
+
 test_108() {
 	# a modification of test 5c
 	local rc=0