360fb0490ed80791d1c6305334bfd2dac1b06d2c
Patrick Farrell <paf@cray.com>
2018-02-05 06:16:58 -0600

    LU-9906 osd: use pagevec for putting pages
    Using a pagevec instead of individual page puts is much
    more efficient.  This should reduce contention on the page
    cache allocation/freeing, which becomes a bottleneck with
    high speed OSTs.
    
    Cray-bug-id: LUS-5670
    Signed-off-by: Patrick Farrell <paf@cray.com>
    Change-Id: Ic15cb8e30887ec55e9348e50af307bfd7108c7e4
    Reviewed-on: https://review.whamcloud.com/30531
    Tested-by: Jenkins
    Reviewed-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Reviewed-by: Alexey Lyashkov <c17817@cray.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/osd-ldiskfs/osd_io.c
+++ lustre/osd-ldiskfs/osd_io.c
@@ -48,6 +48,8 @@
 #include <linux/types.h>
 /* prerequisite for linux/xattr.h */
 #include <linux/fs.h>
+#include <linux/mm.h>
+#include <linux/pagevec.h>
 
 /*
  * struct OBD_{ALLOC,FREE}*()
@@ -464,17 +466,24 @@ struct page *osd_get_page(struct dt_object *dt, loff_t offset, int rw)
 static int osd_bufs_put(const struct lu_env *env, struct dt_object *dt,
 			struct niobuf_local *lnb, int npages)
 {
+	struct pagevec pvec;
 	int i;
+
+	pagevec_init(&pvec, 0);
 
 	for (i = 0; i < npages; i++) {
 		if (lnb[i].lnb_page == NULL)
 			continue;
 		LASSERT(PageLocked(lnb[i].lnb_page));
 		unlock_page(lnb[i].lnb_page);
-		page_cache_release(lnb[i].lnb_page);
+		if (pagevec_add(&pvec, lnb[i].lnb_page) == 0)
+			pagevec_release(&pvec);
 		lu_object_put(env, &dt->do_lu);
 		lnb[i].lnb_page = NULL;
 	}
+
+	/* Release any partial pagevec */
+	pagevec_release(&pvec);
 
 	RETURN(0);
 }
