9f2c6689c2cdc2a27531f1a6f7c776274ad73e96
Alexey Lyashkov <c17817@cray.com>
2018-09-04 05:49:13 +0300

    LUS-6124 tests: don't process orphan inodes
    lfsck should not connect orphan inodes into normal namespace.
    it creates unremovable entries and causes ext4 corruption
    in some cases.
    
    Test-Parameters: testlist=sanity-lfsck/37
    
    Change-Id: I42ec1314eb373fd673bb46496e3c0112634c2222
    Signed-off-by: Alexey Lyashkov <c17817@cray.com>
    Reviewed-on: https://es-gerrit.dev.cray.com/153757
    Reviewed-by: Elena Gryaznova <c17455@cray.com>
    Reviewed-by: Alexander Boyko <c17825@cray.com>
    Tested-by: Vitaly Fertman <c17818@cray.com>

--- lustre/tests/sanity-lfsck.sh
+++ lustre/tests/sanity-lfsck.sh
@@ -5532,6 +5532,29 @@ test_36c() {
 }
 run_test 36c "rebuild LOV EA for mirrored file (3)"
 
+test_37()
+{
+	local pid
+	local rc
+	local t_dir="$DIR/$tdir/d0"
+	check_mount_and_prep
+
+	$LFS mkdir -i 0 $t_dir || error "(2) Fail to mkdir $t_dir on MDT0"
+	multiop_bg_pause $t_dir D_c || error "multiop failed: $?"
+	pid=$!
+	rmdir $t_dir
+
+	$START_NAMESPACE -r -A || {
+	    kill -USR1 $PID; error "(3) Fail to start LFSCK for namespace!";  }
+
+	wait_all_targets_blocked namespace completed 4
+	rc=0
+	stat $t_dir && rc=1
+	kill -USR1 $pid
+	return $rc
+}
+run_test 37 "LFSCK must skip a ORPHAN"
+
 # restore MDS/OST size
 MDSSIZE=${SAVED_MDSSIZE}
 OSTSIZE=${SAVED_OSTSIZE}
