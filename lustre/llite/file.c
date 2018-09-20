lustre/lustre-release

commit db59ecb5d1d0284fb918def6348a11e0966d7767

LU-8964 clio: Parallelize generic I/O
Add parallel version of cl_io_loop() function which use information
about stripes from LOV layer and process them in parallel.
This feature is disabled by default. To enable it you should run
"lctl set_param llite.*.pio=1" command.

IOR results on KNL for:
access             = file-per-process
ordering in a file = sequential offsets
ordering inter file= no tasks offsets
clients            = 1 (1 per node)
repetitions        = 1
blocksize          = 128 GiB
aggregate filesize = 128 GiB

xfsize	pio	Write	Read
16	none	170.46	372.12
16	off	370.46	926.53
16	on	668.49	899.55
32	off	368.75	908.95
32	on	469.54	987.64

IOR results on Broadwell Xeon for:
access             = file-per-process
ordering in a file = sequential offsets
ordering inter file= no tasks offsets
clients            = 1 (1 per node)
repetitions        = 1
xfersize           = 16 MiB
blocksize          = 128 GiB
aggregate filesize = 128 GiB

pio	Write	Read
none	1419.80	1277.88
off	1348.98	2245.84
on	 990.76	2320.08

The scalability IOR results on other Broadwell Xeon for:
access             = file-per-process
ordering in a file = sequential offsets
ordering inter file= no tasks offsets
repetitions        = 1
xfersize           = 4 MiB
blocksize          = 8 GiB

Threads	pio	Write	Read
 32	off	9358.38	2649.28
 32	on	9147.14	2677.44
 64	off	8538.65	2811.05
 64	on	8944.19	2908.44
128	off	7978.61	2937.03
128	on	8613.91	2928.44

The numbers are in ‘MB/s’

Signed-off-by: Dmitry Eremin <dmitry.eremin@intel.com>
Change-Id: Id028faba1726fb377d0e903e8b8095d5ea9d1ee2
Reviewed-on: https://review.whamcloud.com/26468
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Niu Yawei <yawei.niu@intel.com>
Reviewed-by: Jinshan Xiong <jinshan.xiong@intel.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/llite/file.c
@@ -1083,27 +1083,120 @@ static bool file_is_noatime(const struct file *file)
 	return false;
 }
 
-static void ll_io_init(struct cl_io *io, const struct file *file, int write)
+static int ll_file_io_ptask(struct cfs_ptask *ptask);
+
+static void ll_io_init(struct cl_io *io, struct file *file, enum cl_io_type iot)
 {
-	struct inode *inode = file_inode((struct file *)file);
+	struct inode *inode = file_inode(file);
 
-        io->u.ci_rw.crw_nonblock = file->f_flags & O_NONBLOCK;
-	if (write) {
-		io->u.ci_wr.wr_append = !!(file->f_flags & O_APPEND);
-		io->u.ci_wr.wr_sync = file->f_flags & O_SYNC ||
-				      file->f_flags & O_DIRECT ||
-				      IS_SYNC(inode);
+	memset(&io->u.ci_rw.rw_iter, 0, sizeof(io->u.ci_rw.rw_iter));
+	init_sync_kiocb(&io->u.ci_rw.rw_iocb, file);
+	io->u.ci_rw.rw_file = file;
+	io->u.ci_rw.rw_ptask = ll_file_io_ptask;
+	io->u.ci_rw.rw_nonblock = !!(file->f_flags & O_NONBLOCK);
+	if (iot == CIT_WRITE) {
+		io->u.ci_rw.rw_append = !!(file->f_flags & O_APPEND);
+		io->u.ci_rw.rw_sync   = !!(file->f_flags & O_SYNC ||
+					   file->f_flags & O_DIRECT ||
+					   IS_SYNC(inode));
 	}
-        io->ci_obj     = ll_i2info(inode)->lli_clob;
-        io->ci_lockreq = CILR_MAYBE;
-        if (ll_file_nolock(file)) {
-                io->ci_lockreq = CILR_NEVER;
-                io->ci_no_srvlock = 1;
-        } else if (file->f_flags & O_APPEND) {
-                io->ci_lockreq = CILR_MANDATORY;
-        }
-
+	io->ci_obj = ll_i2info(inode)->lli_clob;
+	io->ci_lockreq = CILR_MAYBE;
+	if (ll_file_nolock(file)) {
+		io->ci_lockreq = CILR_NEVER;
+		io->ci_no_srvlock = 1;
+	} else if (file->f_flags & O_APPEND) {
+		io->ci_lockreq = CILR_MANDATORY;
+	}
 	io->ci_noatime = file_is_noatime(file);
+	if (ll_i2sbi(inode)->ll_flags & LL_SBI_PIO)
+		io->ci_pio = !io->u.ci_rw.rw_append;
+	else
+		io->ci_pio = 0;
+}
+
+static int ll_file_io_ptask(struct cfs_ptask *ptask)
+{
+	struct cl_io_pt *pt = ptask->pt_cbdata;
+	struct file *file = pt->cip_file;
+	struct lu_env *env;
+	struct cl_io *io;
+	loff_t pos = pt->cip_pos;
+	int rc;
+	__u16 refcheck;
+	ENTRY;
+
+	env = cl_env_get(&refcheck);
+	if (IS_ERR(env))
+		RETURN(PTR_ERR(env));
+
+	CDEBUG(D_VFSTRACE, "%s: %s range: [%llu, %llu)\n",
+		file_dentry(file)->d_name.name,
+		pt->cip_iot == CIT_READ ? "read" : "write",
+		pos, pos + pt->cip_count);
+
+restart:
+	io = vvp_env_thread_io(env);
+	ll_io_init(io, file, pt->cip_iot);
+	io->u.ci_rw.rw_iter = pt->cip_iter;
+	io->u.ci_rw.rw_iocb = pt->cip_iocb;
+	io->ci_pio = 0; /* It's already in parallel task */
+
+	rc = cl_io_rw_init(env, io, pt->cip_iot, pos,
+			   pt->cip_count - pt->cip_result);
+	if (!rc) {
+		struct vvp_io *vio = vvp_env_io(env);
+
+		vio->vui_io_subtype = IO_NORMAL;
+		vio->vui_fd = LUSTRE_FPRIVATE(file);
+
+		ll_cl_add(file, env, io, LCC_RW);
+		rc = cl_io_loop(env, io);
+		ll_cl_remove(file, env);
+	} else {
+		/* cl_io_rw_init() handled IO */
+		rc = io->ci_result;
+	}
+
+	if (OBD_FAIL_CHECK_RESET(OBD_FAIL_LLITE_PTASK_IO_FAIL, 0)) {
+		if (io->ci_nob > 0)
+			io->ci_nob /= 2;
+		rc = -EIO;
+	}
+
+	if (io->ci_nob > 0) {
+		pt->cip_result += io->ci_nob;
+		iov_iter_advance(&pt->cip_iter, io->ci_nob);
+		pos += io->ci_nob;
+		pt->cip_iocb.ki_pos = pos;
+#ifdef HAVE_KIOCB_KI_LEFT
+		pt->cip_iocb.ki_left = pt->cip_count - pt->cip_result;
+#elif defined(HAVE_KI_NBYTES)
+		pt->cip_iocb.ki_nbytes = pt->cip_count - pt->cip_result;
+#endif
+	}
+
+	cl_io_fini(env, io);
+
+	if ((rc == 0 || rc == -ENODATA) &&
+	    pt->cip_result < pt->cip_count &&
+	    io->ci_need_restart) {
+		CDEBUG(D_VFSTRACE,
+			"%s: restart %s range: [%llu, %llu) ret: %zd, rc: %d\n",
+			file_dentry(file)->d_name.name,
+			pt->cip_iot == CIT_READ ? "read" : "write",
+			pos, pos + pt->cip_count - pt->cip_result,
+			pt->cip_result, rc);
+		goto restart;
+	}
+
+	CDEBUG(D_VFSTRACE, "%s: %s ret: %zd, rc: %d\n",
+		file_dentry(file)->d_name.name,
+		pt->cip_iot == CIT_READ ? "read" : "write",
+		pt->cip_result, rc);
+
+	cl_env_put(env, &refcheck);
+	RETURN(pt->cip_result > 0 ? 0 : rc);
 }
 
 static ssize_t
@@ -1111,39 +1204,45 @@ ll_file_io_generic(const struct lu_env *env, struct vvp_io_args *args,
 		   struct file *file, enum cl_io_type iot,
 		   loff_t *ppos, size_t count)
 {
+	struct range_lock	range;
 	struct vvp_io		*vio = vvp_env_io(env);
 	struct inode		*inode = file_inode(file);
 	struct ll_inode_info	*lli = ll_i2info(inode);
 	struct ll_file_data	*fd  = LUSTRE_FPRIVATE(file);
 	struct cl_io		*io;
+	loff_t			pos = *ppos;
 	ssize_t			result = 0;
 	int			rc = 0;
-	struct range_lock	range;
 
 	ENTRY;
 
-	CDEBUG(D_VFSTRACE, "file: %s, type: %d ppos: %llu, count: %zu\n",
-		file_dentry(file)->d_name.name, iot, *ppos, count);
+	CDEBUG(D_VFSTRACE, "%s: %s range: [%llu, %llu)\n",
+		file_dentry(file)->d_name.name,
+		iot == CIT_READ ? "read" : "write", pos, pos + count);
 
 restart:
 	io = vvp_env_thread_io(env);
-	ll_io_init(io, file, iot == CIT_WRITE);
+	ll_io_init(io, file, iot);
+	if (args->via_io_subtype == IO_NORMAL) {
+		io->u.ci_rw.rw_iter = *args->u.normal.via_iter;
+		io->u.ci_rw.rw_iocb = *args->u.normal.via_iocb;
+	} else {
+		io->ci_pio = 0;
+	}
 
-	if (cl_io_rw_init(env, io, iot, *ppos, count) == 0) {
+	if (cl_io_rw_init(env, io, iot, pos, count) == 0) {
 		bool range_locked = false;
 
 		if (file->f_flags & O_APPEND)
 			range_lock_init(&range, 0, LUSTRE_EOF);
 		else
-			range_lock_init(&range, *ppos, *ppos + count - 1);
+			range_lock_init(&range, pos, pos + count - 1);
 
 		vio->vui_fd  = LUSTRE_FPRIVATE(file);
 		vio->vui_io_subtype = args->via_io_subtype;
 
 		switch (vio->vui_io_subtype) {
 		case IO_NORMAL:
-			vio->vui_iter = args->u.normal.via_iter;
-			vio->vui_iocb = args->u.normal.via_iocb;
 			/* Direct IO reads must also take range lock,
 			 * or multiple reads will try to work on the same pages
 			 * See LU-6227 for details. */
@@ -1169,7 +1268,16 @@ restart:
 		}
 
 		ll_cl_add(file, env, io, LCC_RW);
+		if (io->ci_pio && iot == CIT_WRITE && !IS_NOSEC(inode) &&
+		    !lli->lli_inode_locked) {
+			inode_lock(inode);
+			lli->lli_inode_locked = 1;
+		}
 		rc = cl_io_loop(env, io);
+		if (lli->lli_inode_locked) {
+			lli->lli_inode_locked = 0;
+			inode_unlock(inode);
+		}
 		ll_cl_remove(file, env);
 
 		if (range_locked) {
@@ -1184,22 +1292,31 @@ restart:
 
 	if (io->ci_nob > 0) {
 		result += io->ci_nob;
-		count -= io->ci_nob;
-		*ppos = io->u.ci_wr.wr.crw_pos; /* for splice */
+		count  -= io->ci_nob;
 
-		/* prepare IO restart */
-		if (count > 0 && args->via_io_subtype == IO_NORMAL)
-			args->u.normal.via_iter = vio->vui_iter;
+		if (args->via_io_subtype == IO_NORMAL) {
+			iov_iter_advance(args->u.normal.via_iter, io->ci_nob);
+			pos += io->ci_nob;
+			args->u.normal.via_iocb->ki_pos = pos;
+#ifdef HAVE_KIOCB_KI_LEFT
+			args->u.normal.via_iocb->ki_left = count;
+#elif defined(HAVE_KI_NBYTES)
+			args->u.normal.via_iocb->ki_nbytes = count;
+#endif
+		} else {
+			/* for splice */
+			pos = io->u.ci_rw.rw_range.cir_pos;
+		}
 	}
 out:
 	cl_io_fini(env, io);
 
 	if ((rc == 0 || rc == -ENODATA) && count > 0 && io->ci_need_restart) {
 		CDEBUG(D_VFSTRACE,
-		       "%s: restart %s from %lld, count:%zu, result: %zd\n",
-		       file_dentry(file)->d_name.name,
-		       iot == CIT_READ ? "read" : "write",
-		       *ppos, count, result);
+			"%s: restart %s range: [%llu, %llu) ret: %zd, rc: %d\n",
+			file_dentry(file)->d_name.name,
+			iot == CIT_READ ? "read" : "write",
+			pos, pos + count, result, rc);
 		goto restart;
 	}
 
@@ -1223,7 +1340,11 @@ out:
 		}
 	}
 
-	CDEBUG(D_VFSTRACE, "iot: %d, result: %zd\n", iot, result);
+	CDEBUG(D_VFSTRACE, "%s: %s *ppos: %llu, pos: %llu, ret: %zd, rc: %d\n",
+		file_dentry(file)->d_name.name,
+		iot == CIT_READ ? "read" : "write", *ppos, pos, result, rc);
+
+	*ppos = pos;
 
 	RETURN(result > 0 ? result : rc);
 }
@@ -3007,6 +3128,7 @@ int cl_sync_file_range(struct inode *inode, loff_t start, loff_t end,
 int ll_fsync(struct file *file, loff_t start, loff_t end, int datasync)
 {
 	struct dentry *dentry = file_dentry(file);
+	bool lock_inode;
 #elif defined(HAVE_FILE_FSYNC_2ARGS)
 int ll_fsync(struct file *file, int datasync)
 {
@@ -3031,7 +3153,9 @@ int ll_fsync(struct file *file, struct dentry *dentry, int datasync)
 
 #ifdef HAVE_FILE_FSYNC_4ARGS
 	rc = filemap_write_and_wait_range(inode->i_mapping, start, end);
-	inode_lock(inode);
+	lock_inode = !lli->lli_inode_locked;
+	if (lock_inode)
+		inode_lock(inode);
 #else
 	/* fsync's caller has already called _fdata{sync,write}, we want
 	 * that IO to finish before calling the osc and mdc sync methods */
@@ -3071,7 +3195,8 @@ int ll_fsync(struct file *file, struct dentry *dentry, int datasync)
 	}
 
 #ifdef HAVE_FILE_FSYNC_4ARGS
-	inode_unlock(inode);
+	if (lock_inode)
+		inode_unlock(inode);
 #endif
 	RETURN(rc);
 }

lustre/include/cl_object.h
@@ -89,6 +89,7 @@
  * super-class definitions.
  */
 #include <libcfs/libcfs.h>
+#include <libcfs/libcfs_ptask.h>
 #include <lu_object.h>
 #include <linux/atomic.h>
 #include <linux/mutex.h>
@@ -116,6 +117,8 @@ struct cl_io;
 struct cl_io_slice;
 
 struct cl_req_attr;
+
+extern struct cfs_ptask_engine *cl_io_engine;
 
 /**
  * Device in the client stack.
@@ -1728,10 +1731,21 @@ enum cl_fsync_mode {
 	CL_FSYNC_ALL   = 3
 };
 
-struct cl_io_rw_common {
-        loff_t      crw_pos;
-        size_t      crw_count;
-        int         crw_nonblock;
+struct cl_io_range {
+	loff_t cir_pos;
+	size_t cir_count;
+};
+
+struct cl_io_pt {
+	struct cl_io_pt		*cip_next;
+	struct cfs_ptask	 cip_task;
+	struct kiocb		 cip_iocb;
+	struct iov_iter		 cip_iter;
+	struct file		*cip_file;
+	enum cl_io_type		 cip_iot;
+	loff_t			 cip_pos;
+	size_t			 cip_count;
+	ssize_t			 cip_result;
 };
 
 /**
@@ -1762,15 +1776,16 @@ struct cl_io {
         /** lock requirements, this is just a help info for sublayers. */
         enum cl_io_lock_dmd            ci_lockreq;
         union {
-                struct cl_rd_io {
-                        struct cl_io_rw_common rd;
-                } ci_rd;
-		struct cl_wr_io {
-			struct cl_io_rw_common wr;
-			int                    wr_append;
-			int                    wr_sync;
-		} ci_wr;
-		struct cl_io_rw_common ci_rw;
+		struct cl_rw_io {
+			struct iov_iter		 rw_iter;
+			struct kiocb		 rw_iocb;
+			struct cl_io_range	 rw_range;
+			struct file		*rw_file;
+			unsigned int		 rw_nonblock:1,
+						 rw_append:1,
+						 rw_sync:1;
+			int (*rw_ptask)(struct cfs_ptask *ptask);
+		} ci_rw;
 		struct cl_setattr_io {
 			struct ost_lvb		 sa_attr;
 			unsigned int		 sa_attr_flags;
@@ -1854,7 +1869,9 @@ struct cl_io {
 	/**
 	 * O_NOATIME
 	 */
-			     ci_noatime:1;
+			     ci_noatime:1,
+	/** Set to 1 if parallel execution is allowed for current I/O? */
+			     ci_pio:1;
 	/**
 	 * Number of pages owned by this IO. For invariant checking.
 	 */
@@ -2289,12 +2306,12 @@ int   cl_io_cancel       (const struct lu_env *env, struct cl_io *io,
  */
 static inline int cl_io_is_append(const struct cl_io *io)
 {
-        return io->ci_type == CIT_WRITE && io->u.ci_wr.wr_append;
+	return io->ci_type == CIT_WRITE && io->u.ci_rw.rw_append;
 }
 
 static inline int cl_io_is_sync_write(const struct cl_io *io)
 {
-	return io->ci_type == CIT_WRITE && io->u.ci_wr.wr_sync;
+	return io->ci_type == CIT_WRITE && io->u.ci_rw.rw_sync;
 }
 
 static inline int cl_io_is_mkwrite(const struct cl_io *io)

lustre/include/lustre_compat.h
@@ -41,6 +41,7 @@
 
 #include <libcfs/libcfs.h>
 #include <lustre_patchless_compat.h>
+#include <obd_support.h>
 
 #ifdef HAVE_FS_STRUCT_RWLOCK
 # define LOCK_FS_STRUCT(fs)	write_lock(&(fs)->lock)
@@ -441,5 +442,123 @@ static inline void truncate_inode_pages_final(struct address_space *map)
 #else
 # define GET_POSIX_ACL_XATTR_ENTRY(head) ((head)->a_entries)
 #endif
+
+#ifndef HAVE_IOV_ITER_TRUNCATE
+static inline void iov_iter_truncate(struct iov_iter *i, u64 count)
+{
+	if (i->count > count)
+		i->count = count;
+}
+#endif
+
+#ifndef HAVE_IS_SXID
+static inline bool is_sxid(umode_t mode)
+{
+	return (mode & S_ISUID) || ((mode & S_ISGID) && (mode & S_IXGRP));
+}
+#endif
+
+#ifndef IS_NOSEC
+#define IS_NOSEC(inode)	(!is_sxid(inode->i_mode))
+#endif
+
+#ifndef HAVE_FILE_OPERATIONS_READ_WRITE_ITER
+static inline void iov_iter_reexpand(struct iov_iter *i, size_t count)
+{
+	i->count = count;
+}
+
+static inline struct iovec iov_iter_iovec(const struct iov_iter *iter)
+{
+	return (struct iovec) {
+		.iov_base = iter->iov->iov_base + iter->iov_offset,
+		.iov_len = min(iter->count,
+			       iter->iov->iov_len - iter->iov_offset),
+	};
+}
+
+#define iov_for_each(iov, iter, start)					\
+	for (iter = (start);						\
+	     (iter).count && ((iov = iov_iter_iovec(&(iter))), 1);	\
+	     iov_iter_advance(&(iter), (iov).iov_len))
+
+static inline ssize_t
+generic_file_read_iter(struct kiocb *iocb, struct iov_iter *iter)
+{
+	struct iovec iov;
+	struct iov_iter i;
+	ssize_t bytes = 0;
+
+	iov_for_each(iov, i, *iter) {
+		ssize_t res;
+
+		res = generic_file_aio_read(iocb, &iov, 1, iocb->ki_pos);
+		if (res <= 0) {
+			if (bytes == 0)
+				bytes = res;
+			break;
+		}
+
+		bytes += res;
+		if (res < iov.iov_len)
+			break;
+	}
+
+	if (bytes > 0)
+		iov_iter_advance(iter, bytes);
+	return bytes;
+}
+
+static inline ssize_t
+__generic_file_write_iter(struct kiocb *iocb, struct iov_iter *iter)
+{
+	struct iovec iov;
+	struct iov_iter i;
+	ssize_t bytes = 0;
+
+	/* Since LLITE updates file size at the end of I/O in
+	 * vvp_io_commit_write(), append write has to be done in atomic when
+	 * there are multiple segments because otherwise each iteration to
+	 * __generic_file_aio_write() will see original file size */
+	if (unlikely(iocb->ki_filp->f_flags & O_APPEND && iter->nr_segs > 1)) {
+		struct iovec *iov_copy;
+		int count = 0;
+
+		OBD_ALLOC(iov_copy, sizeof(*iov_copy) * iter->nr_segs);
+		if (!iov_copy)
+			return -ENOMEM;
+
+		iov_for_each(iov, i, *iter)
+			iov_copy[count++] = iov;
+
+		bytes = __generic_file_aio_write(iocb, iov_copy, count,
+						 &iocb->ki_pos);
+		OBD_FREE(iov_copy, sizeof(*iov_copy) * iter->nr_segs);
+
+		if (bytes > 0)
+			iov_iter_advance(iter, bytes);
+		return bytes;
+	}
+
+	iov_for_each(iov, i, *iter) {
+		ssize_t res;
+
+		res = __generic_file_aio_write(iocb, &iov, 1, &iocb->ki_pos);
+		if (res <= 0) {
+			if (bytes == 0)
+				bytes = res;
+			break;
+		}
+
+		bytes += res;
+		if (res < iov.iov_len)
+			break;
+	}
+
+	if (bytes > 0)
+		iov_iter_advance(iter, bytes);
+	return bytes;
+}
+#endif /* HAVE_FILE_OPERATIONS_READ_WRITE_ITER */
 
 #endif /* _LUSTRE_COMPAT_H */

lustre/include/obd_support.h
@@ -531,6 +531,7 @@ extern char obd_jobid_var[];
 #define OBD_FAIL_LLITE_NEWNODE_PAUSE		    0x140a
 #define OBD_FAIL_LLITE_SETDIRSTRIPE_PAUSE	    0x140b
 #define OBD_FAIL_LLITE_CREATE_NODE_PAUSE	    0x140c
+#define OBD_FAIL_LLITE_PTASK_IO_FAIL		    0x140d
 
 
 #define OBD_FAIL_FID_INDIR	0x1501

lustre/llite/llite_internal.h
@@ -46,6 +46,7 @@
 #include <linux/compat.h>
 #include <linux/aio.h>
 
+#include <lustre_compat.h>
 #include "vvp_internal.h"
 #include "range_lock.h"
 
@@ -135,7 +136,8 @@ struct ll_inode_info {
 
 	/* update atime from MDS no matter if it's older than
 	 * local inode atime. */
-	unsigned int	lli_update_atime:1;
+	unsigned int	lli_update_atime:1,
+			lli_inode_locked:1;
 
 	/* Try to make the d::member and f::member are aligned. Before using
 	 * these members, make clear whether it is directory or not. */
@@ -429,6 +431,7 @@ enum stats_track_type {
 				       * suppress_pings */
 #define LL_SBI_FAST_READ     0x400000 /* fast read support */
 #define LL_SBI_FILE_SECCTX   0x800000 /* set file security context at create */
+#define LL_SBI_PIO          0x1000000 /* parallel IO support */
 
 #define LL_SBI_FLAGS { 	\
 	"nolck",	\
@@ -455,6 +458,7 @@ enum stats_track_type {
 	"always_ping",	\
 	"fast_read",	\
 	"file_secctx",	\
+	"pio",		\
 }
 
 /* This is embedded into llite super-blocks to keep track of connect
@@ -1451,121 +1455,4 @@ void cl_inode_fini(struct inode *inode);
 u64 cl_fid_build_ino(const struct lu_fid *fid, int api32);
 u32 cl_fid_build_gen(const struct lu_fid *fid);
 
-#ifndef HAVE_IOV_ITER_TRUNCATE
-static inline void iov_iter_truncate(struct iov_iter *i, u64 count)
-{
-	if (i->count > count)
-		i->count = count;
-}
-#endif
-
-#ifndef HAVE_IS_SXID
-static inline bool is_sxid(umode_t mode)
-{
-	return (mode & S_ISUID) || ((mode & S_ISGID) && (mode & S_IXGRP));
-}
-#endif
-
-#ifndef IS_NOSEC
-#define IS_NOSEC(inode)	(!is_sxid(inode->i_mode))
-#endif
-
-#ifndef HAVE_FILE_OPERATIONS_READ_WRITE_ITER
-static inline void iov_iter_reexpand(struct iov_iter *i, size_t count)
-{
-	i->count = count;
-}
-
-static inline struct iovec iov_iter_iovec(const struct iov_iter *iter)
-{
-	return (struct iovec) {
-		.iov_base = iter->iov->iov_base + iter->iov_offset,
-		.iov_len = min(iter->count,
-			       iter->iov->iov_len - iter->iov_offset),
-	};
-}
-
-#define iov_for_each(iov, iter, start)					\
-	for (iter = (start);						\
-	     (iter).count && ((iov = iov_iter_iovec(&(iter))), 1);	\
-	     iov_iter_advance(&(iter), (iov).iov_len))
-
-static inline ssize_t
-generic_file_read_iter(struct kiocb *iocb, struct iov_iter *iter)
-{
-	struct iovec iov;
-	struct iov_iter i;
-	ssize_t bytes = 0;
-
-	iov_for_each(iov, i, *iter) {
-		ssize_t res;
-
-		res = generic_file_aio_read(iocb, &iov, 1, iocb->ki_pos);
-		if (res <= 0) {
-			if (bytes == 0)
-				bytes = res;
-			break;
-		}
-
-		bytes += res;
-		if (res < iov.iov_len)
-			break;
-	}
-
-	if (bytes > 0)
-		iov_iter_advance(iter, bytes);
-	return bytes;
-}
-
-static inline ssize_t
-__generic_file_write_iter(struct kiocb *iocb, struct iov_iter *iter)
-{
-	struct iovec iov;
-	struct iov_iter i;
-	ssize_t bytes = 0;
-
-	/* Since LLITE updates file size at the end of I/O in
-	 * vvp_io_commit_write(), append write has to be done in atomic when
-	 * there are multiple segments because otherwise each iteration to
-	 * __generic_file_aio_write() will see original file size */
-	if (unlikely(iocb->ki_filp->f_flags & O_APPEND && iter->nr_segs > 1)) {
-		struct iovec *iov_copy;
-		int count = 0;
-
-		OBD_ALLOC(iov_copy, sizeof(*iov_copy) * iter->nr_segs);
-		if (!iov_copy)
-			return -ENOMEM;
-
-		iov_for_each(iov, i, *iter)
-			iov_copy[count++] = iov;
-
-		bytes = __generic_file_aio_write(iocb, iov_copy, count,
-						 &iocb->ki_pos);
-		OBD_FREE(iov_copy, sizeof(*iov_copy) * iter->nr_segs);
-
-		if (bytes > 0)
-			iov_iter_advance(iter, bytes);
-		return bytes;
-	}
-
-	iov_for_each(iov, i, *iter) {
-		ssize_t res;
-
-		res = __generic_file_aio_write(iocb, &iov, 1, &iocb->ki_pos);
-		if (res <= 0) {
-			if (bytes == 0)
-				bytes = res;
-			break;
-		}
-
-		bytes += res;
-		if (res < iov.iov_len)
-			break;
-	}
-
-	if (bytes > 0)
-		iov_iter_advance(iter, bytes);
-	return bytes;
-}
-#endif /* HAVE_FILE_OPERATIONS_READ_WRITE_ITER */
 #endif /* LLITE_INTERNAL_H */

lustre/llite/lproc_llite.c
@@ -926,6 +926,39 @@ ll_fast_read_seq_write(struct file *file, const char __user *buffer,
 }
 LPROC_SEQ_FOPS(ll_fast_read);
 
+static int ll_pio_seq_show(struct seq_file *m, void *v)
+{
+	struct super_block *sb = m->private;
+	struct ll_sb_info *sbi = ll_s2sbi(sb);
+
+	seq_printf(m, "%u\n", !!(sbi->ll_flags & LL_SBI_PIO));
+	return 0;
+}
+
+static ssize_t ll_pio_seq_write(struct file *file, const char __user *buffer,
+				size_t count, loff_t *off)
+{
+	struct seq_file *m = file->private_data;
+	struct super_block *sb = m->private;
+	struct ll_sb_info *sbi = ll_s2sbi(sb);
+	int rc;
+	__s64 val;
+
+	rc = lprocfs_str_to_s64(buffer, count, &val);
+	if (rc)
+		return rc;
+
+	spin_lock(&sbi->ll_lock);
+	if (val == 1)
+		sbi->ll_flags |= LL_SBI_PIO;
+	else
+		sbi->ll_flags &= ~LL_SBI_PIO;
+	spin_unlock(&sbi->ll_lock);
+
+	return count;
+}
+LPROC_SEQ_FOPS(ll_pio);
+
 static int ll_unstable_stats_seq_show(struct seq_file *m, void *v)
 {
 	struct super_block	*sb    = m->private;
@@ -1105,8 +1138,10 @@ struct lprocfs_vars lprocfs_llite_obd_vars[] = {
 	  .fops	=	&ll_root_squash_fops			},
 	{ .name	=	"nosquash_nids",
 	  .fops	=	&ll_nosquash_nids_fops			},
-	{ .name =       "fast_read",
-	  .fops =       &ll_fast_read_fops,                     },
+	{ .name =	"fast_read",
+	  .fops =	&ll_fast_read_fops,			},
+	{ .name =	"pio",
+	  .fops =	&ll_pio_fops,				},
 	{ NULL }
 };
 

lustre/llite/rw26.c
@@ -649,7 +649,7 @@ static int ll_write_begin(struct file *file, struct address_space *mapping,
 	int result = 0;
 	ENTRY;
 
-	CDEBUG(D_VFSTRACE, "Writing %lu of %d to %d bytes\n", index, from, len);
+	CDEBUG(D_PAGE, "Writing %lu of %d to %d bytes\n", index, from, len);
 
 	lcc = ll_cl_find(file);
 	if (lcc == NULL) {
@@ -784,7 +784,7 @@ static int ll_write_end(struct file *file, struct address_space *mapping,
 		if (plist->pl_nr >= PTLRPC_MAX_BRW_PAGES)
 			unplug = true;
 
-		CL_PAGE_DEBUG(D_VFSTRACE, env, page,
+		CL_PAGE_DEBUG(D_PAGE, env, page,
 			      "queued page: %d.\n", plist->pl_nr);
 	} else {
 		cl_page_disown(env, io, page);

lustre/llite/vvp_internal.h
@@ -61,13 +61,7 @@ struct vvp_io {
 	/** super class */
 	struct cl_io_slice     vui_cl;
 	struct cl_io_lock_link vui_link;
-	/**
-	 * I/O vector information to or from which read/write is going.
-	 */
-	struct iov_iter *vui_iter;
-	/**
-	 * Total size for the left IO.
-	 */
+	/** Total size for the left IO. */
 	size_t vui_tot_count;
 
 	union {
@@ -117,7 +111,6 @@ struct vvp_io {
 	* File descriptor against which IO is done.
 	*/
 	struct ll_file_data	*vui_fd;
-	struct kiocb		*vui_iocb;
 
 	/* Readahead state. */
 	pgoff_t	vui_ra_start;

Xyratex/lustre-stable

commit 9eab2e07dd420d33a9d9c56924ba3f7b13621f2e

LUS-6271 llite: aio read returns doubled bytes count
ll_file_io_generic() shouldn't modify ki_left because
it is modified by upper layer.
The regression was introduced by
commit db59ecb5d1d0284fb918def6348a11e0966d7767

Change-Id: I011fc0a1d965871d82dfc9894983deecfb29ee94
Signed-off-by: Andriy Skulysh <c17819@cray.com>
Reviewed-on: https://es-gerrit.dev.cray.com/153672
Reviewed-by: Andrew Perepechko <c17827@cray.com>
Reviewed-by: Alexander Boyko <c17825@cray.com>
Tested-by: Elena Gryaznova <c17455@cray.com>

lustre/llite/file.c
@@ -1413,11 +1413,13 @@ restart:
 				pos += io->ci_nob;
 
 			args->u.normal.via_iocb->ki_pos = pos;
+			if (io->ci_pio) {
 #ifdef HAVE_KIOCB_KI_LEFT
-			args->u.normal.via_iocb->ki_left = count;
+				args->u.normal.via_iocb->ki_left = count;
 #elif defined(HAVE_KI_NBYTES)
-			args->u.normal.via_iocb->ki_nbytes = count;
+				args->u.normal.via_iocb->ki_nbytes = count;
 #endif
+			}
 		} else {
 			/* for splice */
 			pos = io->u.ci_rw.rw_range.cir_pos;