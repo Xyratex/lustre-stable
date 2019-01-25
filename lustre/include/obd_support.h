Cray/lustre

LUS-2034 tests: IR doesn't reconnect after EAGAIN
Check that IR ost-osc reconnects after EAGAIN
despite of disabled pinger_recov.

Test-Parameters: testlist=recovery-small/101b
Change-Id: I35ad57e43825162f4056ad346e22a8dddea0e191
Signed-off-by: Sergey Cheremencev <c17829@cray.com>
Reviewed-on: https://es-gerrit.dev.cray.com/153542
Reviewed-by: Alexey Lyashkov <c17817@cray.com>
Tested-by: Elena Gryaznova <c17455@cray.com>
Reviewed-by: Vitaly Fertman <c17818@cray.com>

lustre/include/obd_support.h
@@ -335,6 +335,7 @@ int obd_alloc_fail(const void *ptr, const char *name, const char *type,
 #define OBD_FAIL_OST_PAUSE_PUNCH         0x236
 #define OBD_FAIL_OST_LIST_ASSERT         0x239
 #define OBD_FAIL_OST_GL_WORK_ALLOC	 0x240
+#define OBD_FAIL_OST_PREPARE_DELAY	 0x243
 
 #define OBD_FAIL_LDLM                    0x300
 #define OBD_FAIL_LDLM_NAMESPACE_NEW      0x301

lustre/tests/recovery-small.sh
@@ -1755,7 +1755,7 @@ test_100()
 }
 run_test 100 "IR: Make sure normal recovery still works w/o IR"
 
-test_101()
+test_101a()
 {
         do_facet mgs $LCTL list_param mgs.*.ir_timeout ||
                 { skip "MGS without IR support"; return 0; }
@@ -1774,7 +1774,28 @@ test_101()
 
         lctl set_param -n osc.$OST1_IMP.pinger_recov=1
 }
-run_test 101 "IR: Make sure IR works w/o normal recovery"
+run_test 101a "IR: Make sure IR works w/o normal recovery"
+
+test_101b()
+{
+        do_facet mgs $LCTL list_param mgs.*.ir_timeout ||
+                { skip "MGS without IR support"; return 0; }
+
+        set_ir_status full
+
+        local OST1_IMP=$(get_osc_import_name client ost1)
+
+#define OBD_FAIL_OST_PREPARE_DELAY	 0x243
+	do_facet ost1 "$LCTL set_param fail_loc=0x243"
+        # disable pinger recovery
+        $LCTL set_param -n osc.$OST1_IMP.pinger_recov=0
+
+#OST may return EAGAIN if it is not configured yet
+        fail ost1
+
+        $LCTL set_param -n osc.$OST1_IMP.pinger_recov=1
+}
+run_test 101b "IR: Make sure IR works w/o normal recovery and proceed EAGAIN"
 
 test_102()
 {

Xyratex/lustre-stable

lustre/include/obd_support.h
@@ -339,6 +339,7 @@ extern char obd_jobid_var[];
 #define OBD_FAIL_OST_STATFS_DELAY	 0x242
 #define OBD_FAIL_OST_INTEGRITY_FAULT	 0x243
 #define OBD_FAIL_OST_INTEGRITY_CMP	 0x244
+#define OBD_FAIL_OST_PREPARE_DELAY	 0x245
 #define OBD_FAIL_OST_DELAY_TRANS	 0x246
 
 #define OBD_FAIL_LDLM                    0x300

lustre/tests/recovery-small.sh
@@ -1737,7 +1737,7 @@ test_100()
 }
 run_test 100 "IR: Make sure normal recovery still works w/o IR"
 
-test_101()
+test_101a()
 {
         do_facet mgs $LCTL list_param mgs.*.ir_timeout ||
                 { skip "MGS without IR support"; return 0; }
@@ -1756,7 +1756,28 @@ test_101()
 
         lctl set_param -n osc.$OST1_IMP.pinger_recov=1
 }
-run_test 101 "IR: Make sure IR works w/o normal recovery"
+run_test 101a "IR: Make sure IR works w/o normal recovery"
+
+test_101b()
+{
+        do_facet mgs $LCTL list_param mgs.*.ir_timeout ||
+                { skip "MGS without IR support"; return 0; }
+
+        set_ir_status full
+
+        local OST1_IMP=$(get_osc_import_name client ost1)
+
+#define OBD_FAIL_OST_PREPARE_DELAY	 0x245
+	do_facet ost1 "$LCTL set_param fail_loc=0x245"
+        # disable pinger recovery
+        $LCTL set_param -n osc.$OST1_IMP.pinger_recov=0
+
+#OST may return EAGAIN if it is not configured yet
+        fail ost1
+
+        $LCTL set_param -n osc.$OST1_IMP.pinger_recov=1
+}
+run_test 101b "IR: Make sure IR works w/o normal recovery and proceed EAGAIN"
 
 test_102()
 {